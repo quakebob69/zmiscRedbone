a208d27b02e0c61143a71cc0e566a3995a0c54e1
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-02-16 09:14:51 -0700

    asdf

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/sql/Gen_PR_DB/Gen_PR_DB.bat
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/sql/Gen_PR_DB/Gen_PR_DB.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "sqlDir=C:\Users\danny.call\zmiscRedbone\aPLATFORM_DEPLOY\1TOBEDEPLOYED\PAPERWORK REQUIRED\sql"
+set "sqlDir=C:\Users\danny.call\zmiscRedbone\aPLATFORM_DEPLOY\1TOBEDEPLOYED\PAPERWORK REQUIRED\sql\Gen_PR_DB\sql"
 
 for %%f in ("%sqlDir%\*.sql") do (
-    	echo. & echo. & echo. & echo.*** "%%f" ***& echo.------------------------------------
+    	echo. & echo. & echo. & echo.*** "%%~nxf" ***& echo.
 		sqlcmd -S .\SQLEXPRESS -E -i "%%f"
 )
 
