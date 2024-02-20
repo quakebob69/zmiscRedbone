4ce31276a6576ae96f0fd11a85c248c9686ff3b6
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-02-20 16:16:50 -0700

    asdf

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/110.0---PayrollOTRPayPeriod___DROPCREATE.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/110.0---PayrollOTRPayPeriod___DROPCREATE.sql
@@ -21,14 +21,11 @@ CREATE TABLE [payroll].[PayrollOTRPayPeriod](
 	[PayrollOTRStatusId] [int] NOT NULL,
 	[BeginDate] [datetime] NOT NULL,
 	[EndDate] [datetime] NOT NULL,
-	[OfficePayTotalAmount] [decimal](18, 2) NULL,
-	[OTRTotalMiles] [int] NULL,
-	[OTRTotalMilesValue] [decimal](18, 2) NULL,
-	[OTRTotalHeldAmount] [decimal](18, 2) NULL,
+	[TotalMiles] [int] NULL,
+	[TotalMilesValue] [decimal](18, 2) NULL,
+	[TotalHeldAmount] [decimal](18, 2) NULL,
 	[PTOTotalHrsAccrual] [int] NULL,
 	[UTOTotalDaysAccrual] [int] NULL,
-	[OTRCSV] [varbinary](max) NULL,
-	[OTRPDF] [varbinary](max) NULL,
 	[LastUpdate] [datetime] NULL,
 	[LastUpdateBy] [int] NULL,
 	[Description] [varchar](256) NULL
@@ -40,7 +37,7 @@ CREATE TABLE [payroll].[PayrollOTRPayPeriod](
 (
 	[Code] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
 ALTER TABLE [payroll].[PayrollOTRPayPeriod]  WITH CHECK ADD  CONSTRAINT [FK_PayrollOTRPayPeriod_PayrollOTRStatus] FOREIGN KEY([PayrollOTRStatusId])
