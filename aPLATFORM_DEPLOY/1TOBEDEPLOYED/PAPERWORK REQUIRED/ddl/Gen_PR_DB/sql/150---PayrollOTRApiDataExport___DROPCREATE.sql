38448af23ddd39bfe12177c3ffb46b2689564436
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-02-20 10:56:14 -0700

    asdf

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/150---PayrollOTRApiDataExport___DROPCREATE.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/150---PayrollOTRApiDataExport___DROPCREATE.sql
@@ -14,7 +14,7 @@ CREATE TABLE [payroll].[PayrollOTRApiDataExport](
 	[PayrollOTRApiDataExportId] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [varchar](128) NOT NULL,
 	[PayrollOTRApiDataExportTypeId] [int] NOT NULL,
-	[PayrollItemId] [int] NOT NULL,
+	[PayrollItemName] [varchar](128) NOT NULL,
 	[Quantity] [decimal](10, 2) NULL,
 	[Rate] [decimal](10, 2) NULL
  CONSTRAINT [PK_PayrollOTRApiDataExport] PRIMARY KEY CLUSTERED 
@@ -22,5 +22,13 @@ CREATE TABLE [payroll].[PayrollOTRApiDataExport](
 	[PayrollOTRApiDataExportId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+
+ALTER TABLE [payroll].[PayrollOTRApiDataExport]  WITH CHECK ADD  CONSTRAINT [FK_PayrollOTRApiDataExport_PayrollOTRApiDataExportType] FOREIGN KEY([PayrollOTRApiDataExportTypeId])
+REFERENCES [payroll].[PayrollOTRApiDataExportType] ([PayrollOTRApiDataExportTypeId])
+GO
+
+ALTER TABLE [payroll].[PayrollOTRApiDataExport] CHECK CONSTRAINT [FK_PayrollOTRApiDataExport_PayrollOTRApiDataExportType]
 GO
 
