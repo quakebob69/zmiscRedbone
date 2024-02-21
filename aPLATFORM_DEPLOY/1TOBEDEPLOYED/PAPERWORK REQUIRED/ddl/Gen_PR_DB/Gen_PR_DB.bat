9e688b87b670b1212d78bb9284ae8107387d6e68
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-02-21 14:09:58 -0700

    asdf

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/Gen_PR_DB.bat
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/Gen_PR_DB.bat
@@ -2,11 +2,10 @@
 setlocal enabledelayedexpansion
 
 rem TEMP TABLES
-cd /d "C:\Users\danny.call\zmiscRedbone\aPLATFORM_DEPLOY\1TOBEDEPLOYED\PAPERWORK REQUIRED\ddl\Gen_PR_DB___TEMP_TABLES\zzztempdannydeletethisafterproject\sql\CREATE\"
-call zzztempdannydeletethisafterproject_____CREATE.bat"
+cd /d "C:\Users\danny.call\zmiscRedbone\aPLATFORM_DEPLOY\1TOBEDEPLOYED\PAPERWORK REQUIRED\ddl\Gen_PR_DB___TEMP_TABLES\"
+call zzztempdannydeletethisafterproject_____CREATE.bat
 
 set "sqlDir=C:\Users\danny.call\zmiscRedbone\aPLATFORM_DEPLOY\1TOBEDEPLOYED\PAPERWORK REQUIRED\ddl\Gen_PR_DB\sql"
-
 for %%f in ("%sqlDir%\*.sql") do (
     	echo. & echo. & echo. & echo.*** "%%~nxf" ***& echo.
 		rem local
