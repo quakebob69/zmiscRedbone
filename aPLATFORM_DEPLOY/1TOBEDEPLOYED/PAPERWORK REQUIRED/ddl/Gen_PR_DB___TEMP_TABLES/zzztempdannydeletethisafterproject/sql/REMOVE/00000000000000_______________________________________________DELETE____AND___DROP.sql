b93e9c8fd757efd158ca4451e93af9c775117ef4
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-02-21 13:33:34 -0700

    sdfg

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB___TEMP_TABLES/zzztempdannydeletethisafterproject/sql/REMOVE/00000000000000_______________________________________________DELETE____AND___DROP.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB___TEMP_TABLES/zzztempdannydeletethisafterproject/sql/REMOVE/00000000000000_______________________________________________DELETE____AND___DROP.sql
@@ -3,6 +3,7 @@ GO
 
 
 
+
 --ALTER TABLE [zzztempdannydeletethisafterproject].[PersonPTO___zzztempdannydeletethisafterproject] DROP CONSTRAINT [FK_PersonPTO___zzztempdannydeletethisafterproject_PersonPTOType]
 --GO
 --ALTER TABLE [zzztempdannydeletethisafterproject].[PersonPTO___zzztempdannydeletethisafterproject] DROP CONSTRAINT [FK_PersonPTO___zzztempdannydeletethisafterproject_Person]
@@ -12,18 +13,20 @@ GO
 
 
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[zzztempdannydeletethisafterproject].[PR_OTR_History___zzztempdannydeletethisafterproject]') AND type in (N'U'))
-	DELETE FROM [zzztempdannydeletethisafterproject].[PR_OTR_History___zzztempdannydeletethisafterproject];
+	--DELETE FROM [zzztempdannydeletethisafterproject].[PR_OTR_History___zzztempdannydeletethisafterproject];
 	DROP TABLE [zzztempdannydeletethisafterproject].[PR_OTR_History___zzztempdannydeletethisafterproject]
 GO
 
 
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[zzztempdannydeletethisafterproject].[PersonPTO___zzztempdannydeletethisafterproject]') AND type in (N'U'))
-	DELETE FROM [zzztempdannydeletethisafterproject].[PersonPTO___zzztempdannydeletethisafterproject];
+	--DELETE FROM [zzztempdannydeletethisafterproject].[PersonPTO___zzztempdannydeletethisafterproject];
 	DROP TABLE [zzztempdannydeletethisafterproject].[PersonPTO___zzztempdannydeletethisafterproject];
 GO
 
 
+--select count(*) from [zzztempdannydeletethisafterproject].[PR_OTR_History___zzztempdannydeletethisafterproject];
 
+--select count(*) from [zzztempdannydeletethisafterproject].[PR_OTR_History___zzztempdannydeletethisafterproject];
 
 
 IF EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'zzztempdannydeletethisafterproject')
