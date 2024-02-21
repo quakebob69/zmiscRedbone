1134e9214f30da7b78f586912d5d37db4a5add98
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-02-21 07:40:55 -0700

    asdf

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/000010---------------------------------------------------------DROP_NEW_DDL.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/000010---------------------------------------------------------DROP_NEW_DDL.sql
@@ -29,34 +29,30 @@
 
 
 --TABLES
-	--'export' schema
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[export].[ExportAccountingCompany]') AND type in (N'U'))
-		DROP TABLE [export].[ExportAccountingCompany]
-	GO
-	
-	
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[export].[ExportAccountingPayrollItem]') AND type in (N'U'))
-		DROP TABLE [export].[ExportAccountingPayrollItem]
-	GO
-	
-
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[export].[ExportAccountingPayrollEntryType]') AND type in (N'U'))
-		DROP TABLE [export].[ExportAccountingPayrollEntryType]
-	GO
-
-
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[export].[ExportAccountingPayrollData]') AND type in (N'U'))
-		DROP TABLE [export].[ExportAccountingPayrollData]
-	GO
-
-
-
-
-	--'payroll' schema
 	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRPaymentHold]') AND type in (N'U'))
 		DROP TABLE [payroll].[PayrollOTRPaymentHold]
 	GO
 
+
+	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[ExportAccountingPayrollData]') AND type in (N'U'))
+		DROP TABLE [payroll].[ExportAccountingPayrollData]
+	GO
+
+	
+	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[ExportAccountingPayrollItem]') AND type in (N'U'))
+		DROP TABLE [payroll].[ExportAccountingPayrollItem]
+	GO
+
+
+	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[ExportAccountingPayrollEntryType]') AND type in (N'U'))
+		DROP TABLE [payroll].[ExportAccountingPayrollEntryType]
+	GO
+
+
+	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[ExportAccountingCompany]') AND type in (N'U'))
+		DROP TABLE [payroll].[ExportAccountingCompany]
+	GO
+	
 
 	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRPaymentHoldReason]') AND type in (N'U'))
 		DROP TABLE [payroll].[PayrollOTRPaymentHoldReason]
