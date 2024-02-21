ef51ef011b2372d542985979a90a1a68764df33b
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-02-21 09:54:38 -0700

    asdf

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/000010---------------------------------------------------------DROP_NEW_DDL.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/000010---------------------------------------------------------DROP_NEW_DDL.sql
@@ -1,76 +1,85 @@
 --STORED PROCEDURES
 	--'payroll' schema 
-	DROP PROCEDURE IF EXISTS [payroll].[sp_PayrollPTO_Admin_Accrual]
-	GO
+		DROP PROCEDURE IF EXISTS [payroll].[sp_PayrollPTO_Admin_Accrual]
+		GO
 
-	DROP PROCEDURE IF EXISTS [payroll].[sp_PayrollPTO_OTR_Accrual]
-	GO
-	
-	DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_StagePayRecords__ALL_SOURCES]
-	GO
-
-	DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_StagePayRecords__Load]
-	GO
-
-	DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_StagePayRecords__DriverPay]
-	GO
-
-	DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_GetPayRecordsFrom__Load]
-	GO
-
-	DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_GetPayRecordsFrom__DriverPay]
-	GO
-
-
+		DROP PROCEDURE IF EXISTS [payroll].[sp_PayrollPTO_OTR_Accrual]
+		GO
+		
+		DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_FinalizePayRecords__ALL_SOURCES]
+		GO
+		
+		DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_FinalizePayRecords__DriverPay]
+		GO
+		  
+		DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_FinalizePayRecords__Load]
+		GO
+			   
+		DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_StagePayRecords__ALL_SOURCES]
+		GO
+		   
+		DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_StagePayRecords__DriverPay]
+		GO
+			 
+		DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_StagePayRecords__Load]
+		GO
+				  
+		DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_GetPayRecordsFrom__DriverPay]
+		GO
+		   
+		DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_GetPayRecordsFrom__Load]
+		GO
+		
 
 --TABLES
 	--'payroll' schema 
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRPaymentHold]') AND type in (N'U'))
-		DROP TABLE [payroll].[PayrollOTRPaymentHold]
-	GO
+		IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRPaymentHold]') AND type in (N'U'))
+			DROP TABLE [payroll].[PayrollOTRPaymentHold]
+		GO
 
 
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRPaymentHoldReason]') AND type in (N'U'))
-		DROP TABLE [payroll].[PayrollOTRPaymentHoldReason]
-	GO
+		IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRPaymentHoldReason]') AND type in (N'U'))
+			DROP TABLE [payroll].[PayrollOTRPaymentHoldReason]
+		GO
 
 
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRStaging]') AND type in (N'U'))
-		DROP TABLE [payroll].[PayrollOTRStaging]
-	GO
+		IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRStaging]') AND type in (N'U'))
+			DROP TABLE [payroll].[PayrollOTRStaging]
+		GO
 
 
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRPayPeriod]') AND type in (N'U'))
-		DROP TABLE [payroll].[PayrollOTRPayPeriod]
-	GO
+		IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRPayPeriod]') AND type in (N'U'))
+			DROP TABLE [payroll].[PayrollOTRPayPeriod]
+		GO
 
 
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRDataSource]') AND type in (N'U'))
-		DROP TABLE [payroll].[PayrollOTRDataSource]
-	GO
+		IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRDataSource]') AND type in (N'U'))
+			DROP TABLE [payroll].[PayrollOTRDataSource]
+		GO
 
 
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRStatus]') AND type in (N'U'))
-		DROP TABLE [payroll].[PayrollOTRStatus]
-	GO
+		IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRStatus]') AND type in (N'U'))
+			DROP TABLE [payroll].[PayrollOTRStatus]
+		GO
 
 
 	--'export' schema 
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[export].[AccountingExportPayrollData]') AND type in (N'U'))
-		DROP TABLE [export].[AccountingExportPayrollData]
-	GO
+		IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[export].[AccountingExportPayrollData]') AND type in (N'U'))
+			DROP TABLE [export].[AccountingExportPayrollData]
+		GO
 
-	
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[export].[AccountingExportPayrollItem]') AND type in (N'U'))
-		DROP TABLE [export].[AccountingExportPayrollItem]
-	GO
-
-
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[export].[AccountingExportPayrollEntryType]') AND type in (N'U'))
-		DROP TABLE [export].[AccountingExportPayrollEntryType]
-	GO
+		
+		IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[export].[AccountingExportPayrollItem]') AND type in (N'U'))
+			DROP TABLE [export].[AccountingExportPayrollItem]
+		GO
 
 
-	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[export].[AccountingExportCompany]') AND type in (N'U'))
-		DROP TABLE [export].[AccountingExportCompany]
-	GO
+		IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[export].[AccountingExportPayrollEntryType]') AND type in (N'U'))
+			DROP TABLE [export].[AccountingExportPayrollEntryType]
+		GO
+
+
+		IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[export].[AccountingExportCompany]') AND type in (N'U'))
+			DROP TABLE [export].[AccountingExportCompany]
+		GO
+
