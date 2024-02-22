49aed31549d640d5e518109bae2b3abf92b3c957
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-02-22 13:23:25 -0700

    ASDF

---
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/203---sp_Payroll_OTR_GetActivePayPeriodId___DROPCREATE.sql
@@ -0,0 +1,28 @@
+/****** Object:  StoredProcedure [payroll].[sp_Payroll_OTR_GetActivePayPeriodId]    Script Date: 2/14/2024 2:10:19 PM ******/
+DROP PROCEDURE IF EXISTS [payroll].[sp_Payroll_OTR_GetActivePayPeriodId]
+GO
+
+/****** Object:  StoredProcedure [payroll].[sp_Payroll_OTR_GetActivePayPeriodId]    Script Date: 2/14/2024 2:10:19 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE procedure [payroll].[sp_Payroll_OTR_GetActivePayPeriodId]
+AS
+/*
+	exec [payroll].[sp_Payroll_OTR_GetActivePayPeriodId]
+*/
+
+SET NOCOUNT ON;
+IF 1=0 BEGIN
+SET FMTONLY OFF
+END
+
+	
+	SELECT 'ASDF'
+
+
+GO
