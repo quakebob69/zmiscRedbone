42e292e3f1a0008dfb45f1e2f507af4cb370e6c3
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-02-22 13:08:28 -0700

    asdf

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/work/reset_staging_data/utilities.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/work/reset_staging_data/utilities.sql
@@ -10,38 +10,31 @@ USE [RedBoneThomas]
 	DECLARE @numberBeforeHistoryRecs INT
 		
 		-------
-			SELECT '---------------------INIT DATA AND START---------------------'
+			SELECT 'INIT DATA AND START------------------------------------------------------------------------------------------------------------------------------'
 			DELETE FROM [dispatch].[PR_OTR_History] WHERE LastUpdate > '2024-02-22 09:11:49.643' AND LastUpdateBy = 2775;
 			DELETE FROM [payroll].[PayrollOTRStaging];
 			SET @numberBeforeHistoryRecs = (SELECT COUNT(*) AS 'NUM history RECS - START' FROM [dispatch].[PR_OTR_History])
-			SELECT COUNT(*) AS 'NUM history RECS - START' FROM [payroll].[PayrollOTRStaging]
+			SELECT COUNT(*) AS 'NUM history RECS - START' FROM [dispatch].[PR_OTR_History]
 			SELECT COUNT(*) AS 'NUM staging RECS - START' FROM [payroll].[PayrollOTRStaging]
 		-------	
 
 		-------	
-			SELECT '------------------------------------------'
-			SELECT '------------------------------------------'
-			SELECT '---------------------STAGE---------------------'
+			SELECT 'STAGE------------------------------------------------------------------------------------------------------------------------------'
 			exec [payroll].[sp_Payroll_OTR_StagePayRecords__DriverPay] @payperiod, 2775
 			SELECT COUNT(*) AS 'NUM history RECS - after staging' FROM [dispatch].[PR_OTR_History]
 			SELECT COUNT(*) AS 'NUM staging RECS - after staging' FROM [payroll].[PayrollOTRStaging]
 		-------	
 
 		-------
-			SELECT '------------------------------------------'
-			SELECT '------------------------------------------'
-			SELECT '---------------------FINALIZE---------------------'
+			SELECT 'FINALIZE------------------------------------------------------------------------------------------------------------------------------'
 			exec [payroll].[sp_Payroll_OTR_FinalizePayPeriod] @payperiod, 2775
 			SELECT COUNT(*) AS 'NUM history RECS - after finalizing' FROM [dispatch].[PR_OTR_History]
 			SELECT COUNT(*) - @numberBeforeHistoryRecs AS 'NUM history ADDED' FROM [dispatch].[PR_OTR_History]
-			
 			SELECT COUNT(*) AS 'NUM staging RECS - after finalizing' FROM [payroll].[PayrollOTRStaging]
 		-------	
 
 		------
-			SELECT '------------------------------------------'
-			SELECT '------------------------------------------'
-			SELECT '---------------------INIT DATA AND END---------------------'
+			SELECT 'INIT DATA AND END------------------------------------------------------------------------------------------------------------------------------'
 			DELETE FROM [dispatch].[PR_OTR_History] WHERE LastUpdate > '2024-02-22 09:11:49.643' AND LastUpdateBy = 2775;
 			DELETE FROM [payroll].[PayrollOTRStaging];
 			SELECT COUNT(*) AS 'NUM history RECS - END' FROM [dispatch].[PR_OTR_History]
