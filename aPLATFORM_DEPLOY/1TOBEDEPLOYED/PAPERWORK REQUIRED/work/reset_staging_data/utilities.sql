5c5c9b9cbdd04142f68e7ad3e0f4d089733312f4
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-02-23 10:30:28 -0700

    asdf

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/work/reset_staging_data/utilities.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/work/reset_staging_data/utilities.sql
@@ -11,7 +11,7 @@ USE [RedBoneThomas]
 	DECLARE @numberBeforeHistoryRecs INT
 --================================================================
 		-------
-			exec [payroll].[sp_Payroll_OTR_PP_Open]
+			exec [payroll].[sp_Payroll_OTR_PayPeriod_Open]
 		-------
 
 		-------
