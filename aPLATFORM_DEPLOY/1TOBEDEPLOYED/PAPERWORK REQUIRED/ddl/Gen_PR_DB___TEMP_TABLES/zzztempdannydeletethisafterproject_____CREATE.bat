cafc95ed0935edfacaf5de2155753d6010bb5060
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-02-23 09:37:19 -0700

    ASDF

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB___TEMP_TABLES/zzztempdannydeletethisafterproject_____CREATE.bat
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB___TEMP_TABLES/zzztempdannydeletethisafterproject_____CREATE.bat
@@ -1,15 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
-
-call zzztempdannydeletethisafterproject_____REMOVE.bat
-
 set "sqlDir=C:\Users\danny.call\zmiscRedbone\aPLATFORM_DEPLOY\1TOBEDEPLOYED\PAPERWORK REQUIRED\ddl\Gen_PR_DB___TEMP_TABLES\zzztempdannydeletethisafterproject\sql\CREATE"
 
-for %%f in ("%sqlDir%\*.sql") do (
-    	echo. & echo. & echo. & echo.*** "%%~nxf" ***& echo.
-		rem local
-			rem sqlcmd -S .\SQLEXPRESS -E -i "%%f"
-		rem remote
-			sqlcmd -S 40.78.62.55 -U redboneDB3 -P Kd2GP#Wlrt9Ws -d RedboneThomas -i "%%f"
-)
 
+echo.********* "START TEMP STUFF" *********************************************************&
+
+	call zzztempdannydeletethisafterproject_____REMOVE.bat
+
+	for %%f in ("%sqlDir%\*.sql") do (
+			echo. & echo. & echo. & echo.*** "%%~nxf" ***& echo.
+			rem local
+				rem sqlcmd -S .\SQLEXPRESS -E -i "%%f"
+			rem remote
+				sqlcmd -S 40.78.62.55 -U redboneDB3 -P Kd2GP#Wlrt9Ws -d RedboneThomas -i "%%f"
+	)
+
+echo. & echo.********* "END TEMP STUFF" *********************************************************& echo. & echo. & echo. & echo. & echo. & echo. & echo. & echo. & echo. & echo. & echo.
