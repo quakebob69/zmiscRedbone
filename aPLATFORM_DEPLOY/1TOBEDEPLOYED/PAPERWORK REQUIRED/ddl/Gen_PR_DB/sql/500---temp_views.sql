992b6ff594400f8ea35a8b7f779cd1344d208423
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-03-21 15:20:00 -0600

    asdf

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/500---temp_views.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/500---temp_views.sql
@@ -12,15 +12,18 @@ FROM [payroll].[PayrollOTRStaging] AS s
 WHERE 
 s.DriverPersonId
 	not in (
-		8, -- asdf 
-		54545454554, -- Al Jones 
-		54545454554, -- asdf 
-		54545454554, -- asdf 
-		54545454554, -- asdf 
-		54545454554, -- asdf 
-		54545454554, -- asdf 
-		54545454554 -- asdf 
-)
+
+
+			select personid from main.Person where
+				(LastName like 'COLE' and firstName like 'JACK') or
+				(LastName like 'JONES' and firstName like 'AL') or
+				(LastName like 'TRIBE' and firstName like 'WILLIAM') or
+				(LastName like 'SOTO' and firstName like 'VALMORE') or
+				(LastName like 'SPENCER' and firstName like 'JOHN') or
+				(LastName like 'NORTON' and firstName like 'JERRY')
+		
+		
+		)
 GO
 
 
@@ -30,13 +33,18 @@ FROM [dispatch].[PR_OTR_History] AS h
 WHERE 
 h.DriverPersonId 
 	not in (
-		8, -- asdf 
-		54545454554, -- Al Jones 
-		54545454554, -- asdf 
-		54545454554, -- asdf 
-		54545454554, -- asdf 
-		54545454554, -- asdf 
-		54545454554, -- asdf 
-		54545454554 -- asdf 
-	)
+
+
+			select personid from main.Person where
+				(LastName like 'COLE' and firstName like 'JACK') or
+				(LastName like 'JONES' and firstName like 'AL') or
+				(LastName like 'TRIBE' and firstName like 'WILLIAM') or
+				(LastName like 'SOTO' and firstName like 'VALMORE') or
+				(LastName like 'SPENCER' and firstName like 'JOHN') or
+				(LastName like 'NORTON' and firstName like 'JERRY')
+		
+		
+		)
 GO
+
+
