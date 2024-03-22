f6bd201f59c9a50417ecaa978310f6c05a74c958
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-03-22 15:31:08 -0600

    asdf

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/205---sp_Payroll_OTR_PayPeriodOpen___DROPCREATE.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/205---sp_Payroll_OTR_PayPeriodOpen___DROPCREATE.sql
@@ -41,21 +41,18 @@ END
 		
 	--Zero out pay period
 		UPDATE [payroll].[PayrollOTRPayPeriod]
-		SET		
-			PayrollOTRStatusId = 1,
-			IsActive = 1,
-			IsOpen = 0,
+		SET
 			IsDataLocked = 0,
 			LoadDataIsStaged = 0,
 			DriverPayDataIsStaged = 0,
-			TotalMiles = 0,
-			TotalMilesValue = 0,
-			TotalHeldAmount = 0,
-			TotalOtherpayrollitemsAmount = 0,
-			UTOTotalDaysAccrual = 0,
-			UTOTotalDaysValue = 0,
-			TotalEarningsAmount = 0,
-			TotalDriverPayAmount = 0
+			--TotalMiles = 0,
+			--TotalMilesValue = 0,
+			--TotalHeldAmount = 0,
+			--TotalOtherpayrollitemsAmount = 0,
+			--UTOTotalDaysAccrual = 0,
+			--UTOTotalDaysValue = 0,
+			--TotalEarningsAmount = 0,
+			--TotalDriverPayAmount = 0
 		WHERE PayrollOTRPayPeriodId = @ActivePayPeriodId
 
 GO
