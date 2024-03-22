3fe9a021e21397f9834468240505505968381889
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-03-22 11:15:17 -0600

    SDFG

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/205---sp_Payroll_OTR_PayPeriodOpen___DROPCREATE.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/205---sp_Payroll_OTR_PayPeriodOpen___DROPCREATE.sql
@@ -43,18 +43,18 @@ END
 		UPDATE [payroll].[PayrollOTRPayPeriod]
 		SET		
 			PayrollOTRStatusId] = 1,
-			[IsActive] = 1,
-			[IsOpen] = 0,
-			[IsDataLocked] = 0,
-			[LoadDataIsStaged] = 0,
-			[DriverPayDataIsStaged] = 0,
-			[TotalMiles] = 0,
-			[TotalMilesValue] = 0,
-			[TotalHeldAmount] = 0,
-			[TotalOtherpayrollitemsAmount] = 0,
-			[UTOTotalDaysAccrual] = 0,
-			[UTOTotalDaysValue] = 0,
-			[TotalEarningsAmount] = 0,
+			[IsActive = 1,
+			[IsOpen = 0,
+			[IsDataLocked = 0,
+			[LoadDataIsStaged = 0,
+			[DriverPayDataIsStaged = 0,
+			[TotalMiles = 0,
+			[TotalMilesValue = 0,
+			[TotalHeldAmount = 0,
+			[TotalOtherpayrollitemsAmount = 0,
+			[UTOTotalDaysAccrual = 0,
+			[UTOTotalDaysValue = 0,
+			[TotalEarningsAmount = 0,
 			[TotalDriverPayAmount] = 0
 		WHERE PayrollOTRPayPeriodId = @ActivePayPeriodId
 
