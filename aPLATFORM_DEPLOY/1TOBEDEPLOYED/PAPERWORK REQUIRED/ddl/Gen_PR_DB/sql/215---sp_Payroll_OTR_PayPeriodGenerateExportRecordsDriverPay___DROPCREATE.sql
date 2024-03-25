c5b1ef1695bb7d15f83f648974170e84120f7a7d
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-03-25 08:53:13 -0600

    Update 215---sp_Payroll_OTR_PayPeriodGenerateExportRecordsDriverPay___DROPCREATE.sql

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/215---sp_Payroll_OTR_PayPeriodGenerateExportRecordsDriverPay___DROPCREATE.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/215---sp_Payroll_OTR_PayPeriodGenerateExportRecordsDriverPay___DROPCREATE.sql
@@ -63,34 +63,14 @@ END
 		)
 
 
+	--
 
 
 
-		DECLARE @PayrollOTRStagingId INT
-		DECLARE @PayrollOTRPayPeriodId INT
-		DECLARE @PayrollOTRDataSourceId INT
 
-		DECLARE @Counter INT
-		SET @Counter = 1
-   
-		DECLARE cur CURSOR FOR
-		SELECT PayrollOTRStagingId, PayrollOTRPayPeriodId, PayrollOTRDataSourceId FROM [payroll].[PayrollOTRStaging]
-    
-		OPEN cur
-		FETCH NEXT FROM cur INTO @PayrollOTRStagingId, @PayrollOTRPayPeriodId, @PayrollOTRDataSourceId
-    
-		WHILE @@FETCH_STATUS = 0 AND @Counter < 3
-		BEGIN
-			INSERT INTO [export].[AccountingExportPayrollData]
-				(PersonId, OriginatingOTRPayPeriodId, AccountingExportPayrollEntryTypeId, AccountingExportPayrollItemId)
-			VALUES
-				(2775, @PayrollOTRPayPeriodId, @Counter, @Counter)
-        
-			SET @Counter = @Counter + 1
-			FETCH NEXT FROM cur INTO @PayrollOTRStagingId, @PayrollOTRPayPeriodId, @PayrollOTRDataSourceId
-		END
-    
-		CLOSE cur
-		DEALLOCATE cur
+
+
+
+
 
 GO
