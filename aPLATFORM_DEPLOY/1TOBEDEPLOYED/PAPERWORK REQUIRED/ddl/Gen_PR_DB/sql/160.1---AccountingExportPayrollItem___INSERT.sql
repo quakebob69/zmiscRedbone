6f473468593a936cee97b3973c1eb079a61a3354
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-03-25 08:35:23 -0600

    f

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/160.1---AccountingExportPayrollItem___INSERT.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/160.1---AccountingExportPayrollItem___INSERT.sql
@@ -2,5 +2,4 @@ DELETE FROM [export].[AccountingExportPayrollItem];
 GO
 
 INSERT INTO [export].[AccountingExportPayrollItem] ([AccountingExportCompanyId], [AccountingExportPayrollEntryTypeId], [Name], [Enabled]) VALUES (1, 1, 'Payroll Item 1', 1);
-INSERT INTO [export].[AccountingExportPayrollItem] ([AccountingExportCompanyId], [AccountingExportPayrollEntryTypeId], [Name], [Enabled]) VALUES (1, 1, 'Payroll Item 2', 1);
 GO
