fb2ce03cf7a8103d4d250ef6458bfa07312b11e7
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-03-27 08:02:55 -0600

    d

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/500---temp_views.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/500---temp_views.sql
@@ -9,44 +9,48 @@ GO
 CREATE VIEW [payroll].[vPayrollOTRStaging___withpersonsremoved] AS
 SELECT *
 FROM [payroll].[PayrollOTRStaging] AS s
+/*
 WHERE 
 s.DriverPersonId
 	not in (
 
 
 			select personid from main.Person 
-				--where
-					--(LastName like 'COLE' and firstName like 'JACK') or
-					--(LastName like 'JONES' and firstName like 'AL') or
-					--(LastName like 'TRIBE' and firstName like 'WILLIAM') or
-					--(LastName like 'SOTO' and firstName like 'VALMORE') or
-					--(LastName like 'SPENCER' and firstName like 'JOHN') or
-					--(LastName like 'NORTON' and firstName like 'JERRY')
+				where
+					(LastName like 'COLE' and firstName like 'JACK') or
+					(LastName like 'JONES' and firstName like 'AL') or
+					(LastName like 'TRIBE' and firstName like 'WILLIAM') or
+					(LastName like 'SOTO' and firstName like 'VALMORE') or
+					(LastName like 'SPENCER' and firstName like 'JOHN') or
+					(LastName like 'NORTON' and firstName like 'JERRY')
 		
 		
 		)
+*/
 GO
 
 
 CREATE VIEW [payroll].[vPR_OTR_History___withpersonsremoved] AS
 SELECT *
 FROM [dispatch].[PR_OTR_History] AS h
+/*
 WHERE 
 h.DriverPersonId 
 	not in (
 
 
 			select personid from main.Person
-				--where
-					--(LastName like 'COLE' and firstName like 'JACK') or
-					--(LastName like 'JONES' and firstName like 'AL') or
-					--(LastName like 'TRIBE' and firstName like 'WILLIAM') or
-					--(LastName like 'SOTO' and firstName like 'VALMORE') or
-					--(LastName like 'SPENCER' and firstName like 'JOHN') or
-					--(LastName like 'NORTON' and firstName like 'JERRY')
+				where
+					(LastName like 'COLE' and firstName like 'JACK') or
+					(LastName like 'JONES' and firstName like 'AL') or
+					(LastName like 'TRIBE' and firstName like 'WILLIAM') or
+					(LastName like 'SOTO' and firstName like 'VALMORE') or
+					(LastName like 'SPENCER' and firstName like 'JOHN') or
+					(LastName like 'NORTON' and firstName like 'JERRY')
 			
 		
 		)
+*/
 GO
 
 
