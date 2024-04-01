359c5880e7a5f5bc9118f4475b68250359c1a42d
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-04-01 14:21:21 -0600

    l

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/216---sp_Payroll_OTR_PayPeriodHoldPayments___DROPCREATE.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/216---sp_Payroll_OTR_PayPeriodHoldPayments___DROPCREATE.sql
@@ -25,24 +25,44 @@ IF 1=0 BEGIN
 SET FMTONLY OFF
 END
 
+	--VARS
+		DECLARE @DataSourceName_LOAD VARCHAR(4)
+		SET @DataSourceName_LOAD = 'LOAD'
+		DECLARE @PayrollOTRDataSourceId_LOAD INT
+			SET @PayrollOTRDataSourceId_LOAD = (SELECT PayrollOTRDataSourceId FROM payroll.PayrollOTRDataSource WHERE Name = @DataSourceName_LOAD)
 
---vars
-	DECLARE @DataSourceName_LOAD VARCHAR(4)
-	SET @DataSourceName_LOAD = 'LOAD'
-	DECLARE @PayrollOTRDataSourceId_LOAD INT
-		SET @PayrollOTRDataSourceId_LOAD = (SELECT PayrollOTRDataSourceId FROM payroll.PayrollOTRDataSource WHERE Name = @DataSourceName_LOAD)
 
-	--HELD LOADIDs
-		SELECT DISTINCT(stag.LoadId)
-		FROM
-			[payroll].[PayrollOTRStaging] stag
-				JOIN
-					[dispatch].[Load] ld
-					ON stag.LoadId = ld.LoadId
+	--GET LOADS TO HOLD
+		DECLARE @LoadsToHold TABLE ( LoadId INT )
+		INSERT INTO @LoadsToHold
+			SELECT
+				DISTINCT(stag.LoadId)
+			FROM
+				[payroll].[PayrollOTRStaging] stag
+					JOIN
+						[dispatch].[Load] ld
+						ON stag.LoadId = ld.LoadId
+			WHERE
+				stag.PayrollOTRDataSourceId = @PayrollOTRDataSourceId_LOAD
+				AND
+				ld.PaperworkRecvdDate IS NULL
+			
+
+	--ISHELD RESET
+		UPDATE
+			[payroll].[PayrollOTRStaging]
+		SET
+			isHeld = 0
+
+
+	--HOLD 'LOAD' AND 'DRIVERPAY' RECORDS
+		UPDATE
+			[payroll].[PayrollOTRStaging]
+		SET
+			isHeld = 1
 		WHERE
-			stag.PayrollOTRDataSourceId = @PayrollOTRDataSourceId_LOAD
-			AND
-			ld.PaperworkRecvdDate IS NULL
-
+			--(LoadId in (SELECT LoadId FROM @LoadsToHold))
+			--OR
+			(TripNumber in (SELECT LoadId FROM @LoadsToHold))
 
 GO
