ef99ae8bb47a0c0e8ba4f2ffbd9a98d75de2d9b0
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-04-04 13:53:25 -0600

    f

--- SQLQuery14.sql
+++ SQLQuery14.sql
@@ -8,16 +8,16 @@
 	from 
 	export.AccountingExportPayrollData exdata where OriginatingOTRPayPeriodId = @OpenPayPeriodId
 
-	select
-	count(*) as 'PAID'
-	from 
-	export.AccountingExportPayrollData exdata where OriginatingOTRPayPeriodId = @OpenPayPeriodId
-		AND PaidOTRPayPeriodId = @OpenPayPeriodId
+		select
+		count(*) as 'PAID'
+		from 
+		export.AccountingExportPayrollData exdata where OriginatingOTRPayPeriodId = @OpenPayPeriodId
+			AND PaidOTRPayPeriodId = @OpenPayPeriodId
 
-	select
-	count(*) as 'HELD'
-	from 
-	export.AccountingExportPayrollData exdata where OriginatingOTRPayPeriodId = @OpenPayPeriodId
-		AND PaidOTRPayPeriodId IS NULL
+		select
+		count(*) as 'HELD'
+		from 
+		export.AccountingExportPayrollData exdata where OriginatingOTRPayPeriodId = @OpenPayPeriodId
+			AND PaidOTRPayPeriodId IS NULL
 
 
