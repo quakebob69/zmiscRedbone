d47d09c5130610d152f7d6ffd638b944252f91c5
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-04-08 09:05:49 -0600

    U

--- test__ExportData.sql
+++ test__ExportData.sql
@@ -3,10 +3,18 @@ GO
 
 	delete from [export].[AccountingExportPayrollData];
 
-
+	--[PayPeriodId] ASC,
+	--[PersonId] ASC,
+	--[AccountingExportPayrollEntryTypeId] ASC,
+	--[AccountingExportPayrollItemId] ASC,
+	--[OriginatingOTRPayPeriodId] ASC
 
 INSERT INTO [export].[AccountingExportPayrollData]
-	(PayPeriodId, PersonId, AccountingExportPayrollEntryTypeId, AccountingExportPayrollItemId, OriginatingOTRPayPeriodId,	PayrollOTRDataSourceId,	PayrollOTRPaymentHoldReasonId, Quantity, LoadId)
+(PayPeriodId, PersonId, AccountingExportPayrollEntryTypeId, AccountingExportPayrollItemId,	OriginatingOTRPayPeriodId, PayrollOTRDataSourceId,	PayrollOTRPaymentHoldReasonId, Quantity,	LoadId)
 VALUES
-	(1,2775,	1,									1,								1,					1,1,							5000,			100)
-
+(1,			2775,		1,									1,								1,							1,							NULL,							5000,		100),
+(2,			2775,		1,									1,								2,							1,							NULL,							5000,		100),
+(2,			2775,		1,									1,								2,							1,							NULL,							5000,		100),
+(3,			2775,		1,									1,								2,							1,							1,						 		5000,		100),
+(3,			2775,		1,									1,								3,							1,							NULL,							5000,		100),
+(NULL,		2775,		1,									1,								3,							1,							1,								5000,		100)
