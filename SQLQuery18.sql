0d2bc9faa00ee8c9e6c0dbf8ae0c186eaa4fad1c
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-04-11 10:38:23 -0600

    d

--- SQLQuery18.sql
+++ SQLQuery18.sql
@@ -94,3 +94,44 @@ SELECT * from export.vAccountingExportPayrollDataPastPDFDetail
 --------------
 --------------
 
+
+--Held
+DROP VIEW IF EXISTS [export].[vAccountingExportPayrollDataPastPDFHeld]
+GO
+
+	CREATE VIEW [export].[vAccountingExportPayrollDataPastPDFHeld] AS
+
+			select TOP (1000000) 
+				CONCAT(pers.firstName, ' ', pers.lastName) AS full_name,
+				typ.Name as type_name,
+				itm.Name as item_name,
+				ppo.Code as period_code_orig,
+				LoadIdOrDriverPayId,
+				PayrollOTRPaymentHoldReasonId,
+				Rate,
+				Hours,
+				Quantity
+			from 
+				export.AccountingExportPayrollData exdata
+				join main.Person pers on exdata.PersonId = pers.PersonId
+				join export.AccountingExportPayrollEntryType typ on exdata.AccountingExportPayrollEntryTypeId = typ.AccountingExportPayrollEntryTypeId
+				join export.AccountingExportPayrollItem itm on exdata.AccountingExportPayrollItemId = itm.AccountingExportPayrollItemId
+				join payroll.PayrollOTRPayPeriod ppo on exdata.OriginatingOTRPayPeriodId = ppo.PayrollOTRPayPeriodId
+			where------------------------------------------------------------------------
+				--(exdata.PersonId = 2451 OR exdata.PersonId =  1252) and
+				OriginatingOTRPayPeriodId = (SELECT
+													TOP 1 PayrollOTRPayPeriodId 
+													FROM [payroll].[PayrollOTRPayPeriod]
+													WHERE
+													IsActive = 0)
+				AND
+				PayPeriodId IS NULL
+			order by 
+				period_code_orig,
+				full_name,
+				typ.Name,
+				itm.Name
+
+	GO
+
+SELECT * from export.vAccountingExportPayrollDataPastPDFHeld
