d404e566c935b4700c2a18347715b65ed854089a
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-04-11 13:26:24 -0600

    d

--- DEMO.sql
+++ DEMO.sql
@@ -1,5 +1,7 @@
+DECLARE @dt NVARCHAR(25)	= '2024-04-04 14:12:18.043';
+
 update dispatch.load  set PaperworkRecvdDate = NULL where loadid = 57009
-update dispatch.load  set PaperworkRecvdDate = 'ASDF' where loadid = 57009
+--update dispatch.load  set PaperworkRecvdDate = @dt where loadid = 57009
 
 
 select PaperworkRecvdDate from dispatch.load where loadid = 57009
