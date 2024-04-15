79c9ffc520944d3a2c52d6b9173d5065d8341f91
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-04-15 10:23:24 -0600

    j

--- qwerty1.sql
+++ qwerty1.sql
@@ -13,3 +13,41 @@ RETURN
 	WHERE
 		(StartDateTime >= DATEADD(day, -8, (select BeginDate from payroll.vPayrollOTRCurrentPayPeriod)))
 )
+
+
+/*
+	SELECT
+		*
+	FROM
+		dispatch.Load
+ where loadid in (
+select loadid from dispatch.LoadStop where loadid in (
+	SELECT
+		 loadid
+	FROM
+		dispatch.LoadStop
+	WHERE
+		StartDateTime is null
+
+
+		)
+		)
+
+		order by LoadId desc
+
+
+
+		
+
+select * from dispatch.LoadStop where loadid in (
+	SELECT
+		 loadid
+	FROM
+		dispatch.LoadStop
+	WHERE
+		StartDateTime is null
+
+		and LoadStopTypeId = 1
+		)
+				order by LoadId desc
+*/
