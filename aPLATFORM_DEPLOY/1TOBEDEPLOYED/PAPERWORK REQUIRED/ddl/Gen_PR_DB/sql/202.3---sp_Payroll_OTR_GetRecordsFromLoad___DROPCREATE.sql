2aea95b98cf48c759faa5b3b3fbc1ec19610bab3
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-04-15 07:49:27 -0600

    h

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/202.3---sp_Payroll_OTR_GetRecordsFromLoad___DROPCREATE.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/202.3---sp_Payroll_OTR_GetRecordsFromLoad___DROPCREATE.sql
@@ -1,3 +1,41 @@
+DROP VIEW IF EXISTS [payroll].[vPayrollOTROpenPayPeriod]
+GO
+
+DROP VIEW IF EXISTS [payroll].[vPayrollOTRCurrentPayPeriod]
+GO
+
+
+CREATE VIEW [payroll].[vPayrollOTRCurrentPayPeriod] AS
+
+	SELECT
+		*
+	FROM
+		[payroll].PayrollOTRPayPeriod
+	WHERE
+		IsActive = 1
+
+GO
+
+CREATE VIEW [payroll].[vPayrollOTROpenPayPeriod] AS
+
+	SELECT
+		*
+	FROM
+		[payroll].PayrollOTRPayPeriod
+	WHERE
+		IsOpen = 1
+
+GO
+
+
+
+
+
+
+
+
+
+
 declare	@PayPeriodStart date
 declare @PayPeriodEnd date
 set	@PayPeriodStart='12/10/2023'
@@ -18,10 +56,10 @@ SET @PayPeriodEnd='12/16/2023'
             ,LoadStopId_Drop =  lsDrop.LoadStopId
             ,LegInd = 1
     from 
-            dispatch.Load l
-            INNER JOIN dispatch.LoadStop lsPick on lsPick.LoadId = l.LoadId and lsPick.LoadStopTypeId = 2
+            dispatch.vDispatchCurrentPeriod.Load l
+            INNER JOIN dispatch.vLoadStopCurrentPeriod lsPick on lsPick.LoadId = l.LoadId and lsPick.LoadStopTypeId = 2
             -- get the next leg or last drop
-            INNER JOIN dispatch.LoadStop lsDrop on lsDrop.LoadStopId = iif(exists(select top 1 LoadStopId from dispatch.LoadStop where LoadId = l.LoadId and LoadStopTypeId = 2 and StopNumber > lsPick.StopNumber order by StopNumber asc)
+            INNER JOIN dispatch.vLoadStopCurrentPeriod lsDrop on lsDrop.LoadStopId = iif(exists(select top 1 LoadStopId from dispatch.LoadStop where LoadId = l.LoadId and LoadStopTypeId = 2 and StopNumber > lsPick.StopNumber order by StopNumber asc)
                                                             ,(select top 1 LoadStopId from dispatch.LoadStop where LoadId = l.LoadId and LoadStopTypeId = 2 and StopNumber > lsPick.StopNumber order by StopNumber)
                                                             ,(SELECT TOP 1 LoadStopId FROM dispatch.LoadStop WHERE LoadId = l.LoadId AND LoadStopTypeId = 3 ORDER BY StopNumber desc))
             inner join main.Person p1 on p1.PersonId = lsPick.Driver1_PersonId
