f2cb78d57807957aec9eb9ccd34864f7e93efe43
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-04-16 07:54:28 -0600

    d

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/850---getLsDropCurrentPayPeriodMinus8Days___DROPCREATE.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/850---getLsDropCurrentPayPeriodMinus8Days___DROPCREATE.sql
@@ -6,11 +6,43 @@ RETURNS TABLE
 AS
 RETURN
 (
+/*
 	SELECT
 		*
 	FROM
 		dispatch.LoadStop
 	WHERE
-		(StartDateTime >= DATEADD(day, -10, (select BeginDate from payroll.vPayrollOTRCurrentPayPeriod)))
+		
+		LoadId > = 
+			IIF(
+					EXISTS (
+						(select top 1 loadid, StartDateTime from dispatch.LoadStop where StartDateTime >= (select BeginDate from payroll.vPayrollOTRCurrentPayPeriod))
+					)
+					<=
+					3
+					--EXISTS (
+					--	(select top 1 loadid, DropStartDateTime from dispatch.LoadStop where DropStartDateTime >= (select BeginDate from payroll.vPayrollOTRCurrentPayPeriod))
+					--)		
+					,1,2
+					--, (select top 1 loadid, StartDateTime from dispatch.LoadStop where StartDateTime >= (select BeginDate from payroll.vPayrollOTRCurrentPayPeriod))
+					--, (select top 1 loadid, DropStartDateTime from dispatch.LoadStop where DropStartDateTime >= (select BeginDate from payroll.vPayrollOTRCurrentPayPeriod))
+				--)
+				)
+
+
+
+		--(StartDateTime >= DATEADD(day, -2, ()))
+		--IIF(select BeginDate from payroll.vPayrollOTRCurrentPayPeriod < , true_value, false_value)
 		--LoadId >= 56201 and LoadId <= 56769 
+		*/
+
+
+	SELECT c.LoadId, c.StartDateTime, c.DropStartDateTime
+    FROM dispatch.LoadStop c
+    WHERE c.LoadId = IIF((SELECT s.LOAdid FROM dispatch.LoadStop s WHERE s.LOAdid = 1) = 1, 1, 0)
+
 )
+
+
+
+
