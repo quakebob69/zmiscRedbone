da8461d9cf1f01793cfecbca35e94cb3dcf78af3
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-04-16 08:09:20 -0600

    Update 850---getLsDropCurrentPayPeriodMinus8Days___DROPCREATE.sql

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/850---getLsDropCurrentPayPeriodMinus8Days___DROPCREATE.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/850---getLsDropCurrentPayPeriodMinus8Days___DROPCREATE.sql
@@ -6,7 +6,7 @@ RETURNS TABLE
 AS
 RETURN
 (
-/*
+
 	SELECT
 		*
 	FROM
@@ -33,39 +33,10 @@ RETURN
 		--(StartDateTime >= DATEADD(day, -2, ()))
 		--IIF(select BeginDate from payroll.vPayrollOTRCurrentPayPeriod < , true_value, false_value)
 		--LoadId >= 56201 and LoadId <= 56769 
-		*/
-
-
-		/*
-	SELECT c.LoadId, c.StartDateTime, c.DropStartDateTime
-    FROM dispatch.LoadStop c
-    WHERE c.LoadId =
-						IIF((SELECT s.LOAdid FROM dispatch.LoadStop s WHERE s.LOAdid = 1) = 1,
-						
-						
-						1,
-						
-						
-						0
-	
-	
-						)*/
 
 
 
 
-		/*				
-    SELECT
-		*
-    FROM
-		dispatch.LoadStop
-    WHERE
-		LoadId =
-				IIF(
-					(select top 1 loadid from dispatch.LoadStop s WHERE s.LOAdid = 1) > (SELECT top 1 s.LOAdid FROM dispatch.LoadStop s WHERE s.LOAdid = 1)
-					,(SELECT top 1 s.loadid FROM dispatch.LoadStop s WHERE s.loadid = 1)
-					,(SELECT top 1 s.loadid FROM dispatch.LoadStop s WHERE s.loadid = 1)
-				)*/
 )
 
 
