eda23a45a1ee9f1a8ab8716cbf11e51bd39ee431
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-04-17 13:21:55 -0600

    f

--- QWERTY.sql
+++ QWERTY.sql
@@ -1,29 +1,43 @@
+/*
+	LEFT OUTER JOIN
+		main.ClientAddress claddrorigin
+	ON
+		claddrorigin.ClientId =
+								  IIF
+										(
+										@LegInd = 0
+									
+										,(SELECT TOP (1) ClientId FROM main.ClientAddress WHERE (
+																									UseForBillingInd = 1
+																									AND
+																									ClientId = 
+																												(
+																													SELECT TOP (1) ClientId FROM #CurrentPayPeriodLoadStop WHERE (LoadId = l.LoadId) AND (LoadStopTypeId = 1) AND (StopTypeNumber = 1)
+																												)
+																								)
+										)
+									
+										,lsPick.ClientId
+										) 
+	And
+		claddrorigin.AddressTypeId = 2
+*/																																													
+																																														
+									
+									
 
 
-LEFT OUTER JOIN
-	main.ClientAddress claddrorigin
-ON
-	claddrorigin.ClientId =
-							  IIF
-									(
-									@LegInd = 0
-									
-									,(SELECT TOP (1) ClientId FROM main.ClientAddress WHERE (
-																								UseForBillingInd = 1
-																								AND
-																								ClientId = 
-																											(
-																												SELECT TOP (1) ClientId FROM #CurrentPayPeriodLoadStop WHERE (LoadId = l.LoadId) AND (LoadStopTypeId = 1) AND (StopTypeNumber = 1)
-																											)
-																							)
-									)
-									
-									,lsPick.ClientId
-									) 
-And
-	claddrorigin.AddressTypeId = 2
-																																														
-																																														
-																																														
-																																														
-																																														
+
+										
+
+SELECT TOP (1) ClientId FROM main.ClientAddress WHERE (
+															UseForBillingInd = 1
+															AND
+															ClientId = 
+																		(
+																			SELECT TOP (1) ClientId FROM 
+																			Dispatch.Load l
+																			asdf
+																			WHERE (l.LoadId = 56888) AND (LoadStopTypeId = 1) AND (StopTypeNumber = 1)
+																		)
+														)
