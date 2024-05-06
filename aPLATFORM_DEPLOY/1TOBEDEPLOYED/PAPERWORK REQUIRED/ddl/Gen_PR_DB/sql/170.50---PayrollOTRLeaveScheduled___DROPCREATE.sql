e88cdb490a80b3641f0898542ffa1d010b2c3bf9
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-05-06 17:57:18 -0600

    d

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/170.50---PayrollOTRLeaveScheduled___DROPCREATE.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/170.50---PayrollOTRLeaveScheduled___DROPCREATE.sql
@@ -1,25 +1,25 @@
-/****** Object:  Table [payroll].[PayrollOTRTimeOffScheduled]    Script Date: 2/20/2024 9:39:54 AM ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[PayrollOTRTimeOffScheduled]') AND type in (N'U'))
-DROP TABLE [payroll].[PayrollOTRTimeOffScheduled]
+/****** Object:  Table [payroll].[Leave]    Script Date: 2/20/2024 9:39:54 AM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[payroll].[Leave]') AND type in (N'U'))
+DROP TABLE [payroll].[Leave]
 GO
 
-/****** Object:  Table [payroll].[PayrollOTRTimeOffScheduled]    Script Date: 2/20/2024 9:39:54 AM ******/
+/****** Object:  Table [payroll].[Leave]    Script Date: 2/20/2024 9:39:54 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [payroll].[PayrollOTRTimeOffScheduled](
-	[PayrollOTRTimeOffScheduledId] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [payroll].[Leave](
+	[LeaveId] [int] IDENTITY(1,1) NOT NULL,
 	[PersonId] [int] NOT NULL,
 	[Date] [datetime] NOT NULL,
 	[Hours] [decimal](10, 2) NOT NULL,
 	[Days] [decimal](10, 2) NOT NULL,
- CONSTRAINT [PK_PayrollOTRTimeOffScheduled] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_Leave] PRIMARY KEY CLUSTERED 
 (
-	[PayrollOTRTimeOffScheduledId] ASC
+	[LeaveId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
- CONSTRAINT [UQ_PayrollOTRTimeOffScheduled] UNIQUE NONCLUSTERED 
+ CONSTRAINT [UQ_Leave] UNIQUE NONCLUSTERED 
 (
 	[PersonId],
 	[Date]
@@ -27,8 +27,8 @@ CREATE TABLE [payroll].[PayrollOTRTimeOffScheduled](
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [payroll].[PayrollOTRTimeOffScheduled]  WITH CHECK ADD  CONSTRAINT [FK_PayrollOTRTimeOffScheduled_Person] FOREIGN KEY([PersonId])
+ALTER TABLE [payroll].[Leave]  WITH CHECK ADD  CONSTRAINT [FK_Leave_Person] FOREIGN KEY([PersonId])
 REFERENCES [main].[Person] ([PersonId])
 GO
-ALTER TABLE [payroll].[PayrollOTRTimeOffScheduled] CHECK CONSTRAINT [FK_PayrollOTRTimeOffScheduled_Person]
+ALTER TABLE [payroll].[Leave] CHECK CONSTRAINT [FK_Leave_Person]
 GO
