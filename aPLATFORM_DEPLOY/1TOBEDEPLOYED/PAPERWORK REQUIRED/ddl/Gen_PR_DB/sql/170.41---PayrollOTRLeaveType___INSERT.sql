d2b76e3d75cddeeaa12196b6c8c9aff40a438b28
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-05-06 17:55:17 -0600

    g

---
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/170.41---PayrollOTRLeaveType___INSERT.sql
@@ -0,0 +1,4 @@
+INSERT INTO [payroll].[PayrollOTRLeaveType] ([PayrollOTRLeaveTypeId] ,[Type], [Description], [Enabled]) VALUES (1,'Salary' ,'Salary' ,1)
+INSERT INTO [payroll].[PayrollOTRLeaveType] ([PayrollOTRLeaveTypeId] ,[Type], [Description], [Enabled]) VALUES (2,'OTR' ,'OTR' ,1)
+INSERT INTO [payroll].[PayrollOTRLeaveType] ([PayrollOTRLeaveTypeId] ,[Type], [Description], [Enabled]) VALUES (3,'FedEx' ,'FedEx' ,1)
+
