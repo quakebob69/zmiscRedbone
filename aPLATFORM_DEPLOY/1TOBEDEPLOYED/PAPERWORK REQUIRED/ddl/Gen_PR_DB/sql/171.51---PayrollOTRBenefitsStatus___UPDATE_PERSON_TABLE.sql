d11cac00cd12ce4228dde9e53d988d529ae8fbff
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-05-10 12:08:48 -0600

    d

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/171.51---PayrollOTRBenefitsStatus___UPDATE_PERSON_TABLE.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/PAPERWORK REQUIRED/ddl/Gen_PR_DB/sql/171.51---PayrollOTRBenefitsStatus___UPDATE_PERSON_TABLE.sql
@@ -5,6 +5,10 @@ SET @FullTimeStatusId =
 		SELECT PayrollOTRBenefitsStatusId from [payroll].[PayrollOTRBenefitsStatus] where Name = 'Full-Time'
 	)
 
+
+--SELECT * from [payroll].[PayrollOTRBenefitsStatus] where PayrollOTRBenefitsStatusid = @FullTimeStatusId
+
+
 --All
 UPDATE [main].[Person] SET PayrollOTRBenefitsStatusId = @FullTimeStatusId
 WHERE personid in (select personid from main.person WHERE IsActive = 1)
