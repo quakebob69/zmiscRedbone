3c563b2af14aa387f80748ad0f7c6fae10151774
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-05-13 13:46:04 -0600

    f

--- aPLATFORM_DEPLOY/2DEPLOYED/drvierpointsdollarvalue/ADD_TO_DRIVERPOINTS_TABLE.sql
+++ aPLATFORM_DEPLOY/2DEPLOYED/drvierpointsdollarvalue/ADD_TO_DRIVERPOINTS_TABLE.sql
@@ -7,9 +7,11 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER TABLE [RedBone].[main].[DriverPoint] DROP CONSTRAINT IF EXISTS DF_maindriverpoint_Cost
-ALTER TABLE [RedBone].[main].[DriverPoint] DROP COLUMN IF EXISTS [Cost];
+--ALTER TABLE [RedBone].[main].[DriverPoint] DROP CONSTRAINT IF EXISTS DF_maindriverpoint_Cost
+--ALTER TABLE [RedBone].[main].[DriverPoint] DROP COLUMN IF EXISTS [Cost];
 
 ALTER TABLE [main].[DriverPoint]
 ADD [Cost] [decimal](18, 2) NOT NULL CONSTRAINT DF_maindriverpoint_Cost DEFAULT 0.00
 GO
+
+
