898d0c92c70c0c12a8e9bc9f021c456b5f614702
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-05-22 14:26:07 -0600

    h

--- DEMO.sql
+++ DEMO.sql
@@ -1,15 +1,15 @@
 DECLARE @dt NVARCHAR(25)	= '2024-04-04 14:12:18.043';
 DECLARE @LOADYLOAD INT		= 57009;
 
-select * from [export].[AccountingExportPayrollData] aepd order by LoadIdOrDriverPayId 
+-------------------------------------------------------select * from [export].[AccountingExportPayrollData] aepd order by LoadIdOrDriverPayId 
 
 --reset
---update dispatch.load  set PaperworkRecvdDate = NULL where loadid = 57009
+--update dispatch.load  set PaperworkRecvdDate = NULL where loadid = @LOADYLOAD
 
 
 
 --unhold
---update dispatch.load  set PaperworkRecvdDate = @dt where loadid = 57009
+update dispatch.load set PaperworkRecvdDate = @dt where loadid = @LOADYLOAD
 
 
 select aepd.* , '-------',
@@ -19,7 +19,6 @@ from
 join dispatch.load ld on aepd.LoadIdOrDriverPayId = ld.loadid
 where PayPeriodId is null
 and ld.PaperworkRecvdDate is not null
---where LoadIdOrDriverPayId = 57009 
 
 
 
