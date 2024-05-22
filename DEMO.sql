bbc7b00b46e3df3e948d4260ecf56a8a6834f8b0
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-05-22 13:44:40 -0600

    d

--- DEMO.sql
+++ DEMO.sql
@@ -1,13 +1,30 @@
 DECLARE @dt NVARCHAR(25)	= '2024-04-04 14:12:18.043';
+DECLARE @LOADYLOAD INT		= 57009;
 
+
+
+
+--reset
 --update dispatch.load  set PaperworkRecvdDate = NULL where loadid = 57009
-update dispatch.load  set PaperworkRecvdDate = @dt where loadid = 57009
-
-
-select PaperworkRecvdDate from dispatch.load where loadid = 57009
 
 
 
+--unhold
+--update dispatch.load  set PaperworkRecvdDate = @dt where loadid = 57009
+
+
+select aepd.* 
+from
+[export].[AccountingExportPayrollData] aepd
+join dispatch.load ld on aepd.LoadIdOrDriverPayId = ld.loadid
+where PayPeriodId is null
+and ld.PaperworkRecvdDate is not null
+--where LoadIdOrDriverPayId = 57009 
+
+
+
+
+--select PaperworkRecvdDate from dispatch.load where loadid = 57009
 --select top 1000 * from [dispatch].[PR_OTR_History] order by loadid desc
 --select * from [dispatch].[Load] where  loadid = 57009
 --select * from [dispatch].[PR_OTR_History] where  loadid = 57009
