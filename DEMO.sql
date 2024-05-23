4b4b9c24b09b5aa1cf3c1c8ca4317b4c717db7eb
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-05-23 07:02:01 -0600

    k

--- DEMO.sql
+++ DEMO.sql
@@ -4,17 +4,25 @@ DECLARE @LOADYLOAD INT		= 57009;
 -------------------------------------------------------select * from [export].[AccountingExportPayrollData] aepd order by LoadIdOrDriverPayId 
 
 --reset
---update dispatch.load  set PaperworkRecvdDate = NULL where loadid = @LOADYLOAD
+update dispatch.load  set PaperworkRecvdDate = NULL where loadid = @LOADYLOAD
 
 
 
 --unhold
-update dispatch.load set PaperworkRecvdDate = @dt where loadid = @LOADYLOAD
+--update dispatch.load set PaperworkRecvdDate = @dt where loadid = @LOADYLOAD
 
 
 
 
---select PaperworkRecvdDate from dispatch.load where loadid = 57009
+
+
+select PaperworkRecvdDate from dispatch.load where loadid = @LOADYLOAD
+
+
+
+
+
+
 --select top 1000 * from [dispatch].[PR_OTR_History] order by loadid desc
 --select * from [dispatch].[Load] where  loadid = 57009
 --select * from [dispatch].[PR_OTR_History] where  loadid = 57009
