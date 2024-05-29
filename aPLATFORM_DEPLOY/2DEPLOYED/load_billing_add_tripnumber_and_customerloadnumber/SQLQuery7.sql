2255a65db07fb34fb019ba4d88b73884a953b574
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-05-29 15:09:08 -0600

    d

--- aPLATFORM_DEPLOY/2DEPLOYED/load_billing_add_tripnumber_and_customerloadnumber/SQLQuery7.sql
+++ aPLATFORM_DEPLOY/2DEPLOYED/load_billing_add_tripnumber_and_customerloadnumber/SQLQuery7.sql
@@ -1,2 +1,5 @@
+ LoadId desc
 
-select top 1000 CustomerLoadNumber, LoadId from dispatch.load order by LoadId desc
+
+--select loadid,TripNumber from dispatch.Load where loadid = 12869
+--select loadid,TripNumber from dispatch.Load where TripNumber = 50225
