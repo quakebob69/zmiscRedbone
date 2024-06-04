2f7660aaa6f63de2ccee95f3e28ee4abd3d572d6
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-06-04 11:54:38 -0600

    d

--- aPLATFORM_DEPLOY/2DEPLOYED/person_cert_john/SQLQuery1.sql
+++ aPLATFORM_DEPLOY/2DEPLOYED/person_cert_john/SQLQuery1.sql
@@ -7,7 +7,8 @@ FROM            main.CertificationPersonMapping
 WHERE        (main.CertificationPersonMapping.PersonId = 2775) and activeind = 0)
 */
 
-select * from main.CertificationPersonMapping
+
+/*select * from main.CertificationPersonMapping
                          where CertificationPersonMappingId
 
 						 in (SELECT        CertificationPersonMappingId
@@ -18,5 +19,12 @@ select * from main.CertificationPersonMapping
 
 						 in (SELECT        CertificationPersonMappingId
 FROM            main.CertificationPersonMapping
-WHERE        (main.CertificationPersonMapping.PersonId = 2775) and activeind = 1)
+WHERE        (main.CertificationPersonMapping.PersonId = 2775) and activeind = 1)*/
 
+--update main.CertificationPersonMapping set filename = 'Glossary of Transportation Terms.pdf' where CertificationPersonMappingId = 7643 
+
+--select * from main.CertificationPersonMapping where CertificationPersonMappingId = 7643
+
+
+select * from main.CertificationPersonMapping
+where filename is not null
