78e5c5e1fa245f1d035a7fd8943a05673b10c403
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-06-06 11:25:29 -0600

    D

--- SQLQuery8.sql
+++ SQLQuery8.sql
@@ -11,9 +11,19 @@ from
 	dispatch.Load l
 	join dispatch.LoadStop ls on l.LoadId = ls.LoadId
 	join main.ClientAddress ca on ca.clientid = ls.clientid
-where
+--where
 	--l.loadid in (select Loadid from payroll.PayrollOTRStaging)
-	l.LoadId in (57013)
+	--l.LoadId in (57013)
 order by
 	l.loadid,
 	ls.StopNumber
+
+
+SELECT  top 100 dispatch.[Load].LoadId, dispatch.LoadStop.StopNumber, dispatch.LoadStop.LoadStopTypeId, dispatch.[Load].Driver1_PersonId AS DriverFromLoadRec, main.ClientAddress.State AS PickOrDropState, 
+                  dispatch.LoadStop.Driver1_PersonId AS DriverFromStopRec, dispatch.LoadStop.StartDateTime AS PickOrDropDt, dispatch.LoadStop.DropStartDateTime as PickOrDropDtLeg
+FROM     dispatch.[Load] INNER JOIN
+                  dispatch.LoadStop ON dispatch.[Load].LoadId = dispatch.LoadStop.LoadId INNER JOIN
+                  main.ClientAddress ON dispatch.LoadStop.ClientId = main.ClientAddress.ClientId
+order by
+	dispatch.Load.LoadId,
+	dispatch.LoadStop.StopNumber
