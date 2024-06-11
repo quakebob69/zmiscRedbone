be9d41c4525fa678d92cbc2dfd7f039a0e566f01
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-06-11 15:12:39 -0600

    l

--- chains.sql
+++ chains.sql
@@ -268,55 +268,7 @@ select 'XXXXXXXXXXXXXXXXXXXXX', 'XXXXXXXXXXXXXXXXXXXXX', 'XXXXXXXXXXXXXXXXXXXXX'
 
 
 
---loadstop
-select 'loadstop---' as 'loadstop', loadid, s.driver1_personid, StartDateTime as X, DropStartDateTime as '---------DropX---------NOT IN HISTORY?'
-from
-	dispatch.loadstop s
-where
-	(s.LoadStopTypeId <> 4) and 
-	s.loadid 
-	in (select distinct loadid from payroll.PayrollOTRdriverloadchain)
-	--= 56480
-order by 
-	loadid,
-	stopnumber
-
-
-
-select 
-	'history---' as 'history', loadid, driverpersonid, h.pickupby, h.DeliverBy
-from
-	dispatch.PR_OTR_History h
-where
-	loadid 
-	in (select distinct loadid from payroll.PayrollOTRdriverloadchain)
-	--= 56480
-	and paycode = 'Per Diem'
-order by
-	h.PickupBy
-
-
-
-
-select
-	'chain---' as 'chain', loadid, personid, begindate, enddate
-from
-	payroll.PayrollOTRdriverloadchain
-where
-	loadid 
-	in (select distinct loadid from payroll.PayrollOTRdriverloadchain)
-	--= 56480
-order by 
-	loadid,
-	enddate
-
-
-
-
-	-----------------------------------------------------------------------
-
-
-
+asdf
 
 
 
