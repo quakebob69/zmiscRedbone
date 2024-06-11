26baa7aa3bd4c3772a34bf2206699c406812b200
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-06-11 09:46:53 -0600

    d

--- SQLQuery23.sql
+++ SQLQuery23.sql
@@ -28,8 +28,7 @@ ORDER by
 	personid, enddate
 
 
-	select count(distinct loadid) from dispatch.loadstop where loadid in 
-
+select count(distinct loadid) from dispatch.loadstop where loadid in 
 (
 	select
 	loadid
@@ -40,3 +39,46 @@ from
 	)
 
 	and dispatch.loadstop.LoadStopTypeId = 2;
+
+
+
+
+
+select loadid, count(loadid) as numlegs from dispatch.loadstop
+	where loadid in 
+			(
+				select
+					loadid
+				from
+					payroll.PayrollOTRdriverloadchain
+			)
+
+	and
+		dispatch.loadstop.LoadStopTypeId = 2
+	group by
+		loadid
+	having
+		count(loadid) = 1
+		;
+
+/*
+--1 leg
+	asdf
+
+--2 legs:
+	56547
+	56572
+	56616
+	56634
+	56681
+	56809
+	57171
+	57210
+	57234
+	*/
+
+
+
+
+
+
