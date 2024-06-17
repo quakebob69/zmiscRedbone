93ff9692f444e49dae6d70b5335e8cb6b3165e51
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-06-17 08:00:25 -0600

    D

--- redbone_analysis.sql
+++ redbone_analysis.sql
@@ -1,13 +1,33 @@
 declare @tblName varchar(100)
-set @tblName = 'LOAD'
+
+--DETAIL
+	set @tblName = 'LOAD'
+	--LOAD
+		SELECT
+			OBJECT_NAME(referenced_id) AS referenced_object_name
+			,dep_on.type_desc AS referenced_object_type
+			,OBJECT_NAME(referencing_id) AS referencing_object_name
+			,o.type_desc AS referencing_object_type
+		FROM
+			sys.sql_expression_dependencies sed
+		JOIN
+			sys.objects o ON sed.referencing_id = o.object_id
+		JOIN
+			sys.objects dep_on ON sed.referenced_id = dep_on.object_id
+		WHERE
+			OBJECT_NAME(referenced_id) = @tblName
+		ORDER BY
+			referencing_object_type
+			, referencing_object_name
+		;
 
 
---LOAD
+	set @tblName = 'LOADSTOP'
 	SELECT
-		OBJECT_NAME(referencing_id) AS referencing_object_name,
-		o.type_desc AS referencing_object_type--,
-		--OBJECT_NAME(referenced_id) AS referenced_object_name,
-		--dep_on.type_desc AS referenced_object_type
+			OBJECT_NAME(referenced_id) AS referenced_object_name
+			,dep_on.type_desc AS referenced_object_type
+			,OBJECT_NAME(referencing_id) AS referencing_object_name
+			,o.type_desc AS referencing_object_type
 	FROM
 		sys.sql_expression_dependencies sed
 	JOIN
@@ -22,24 +42,6 @@ set @tblName = 'LOAD'
 	;
 
 
-SELECT
-    OBJECT_NAME(referencing_id) AS referencing_object_name,
-    o.type_desc AS referencing_object_type--,
-    --OBJECT_NAME(referenced_id) AS referenced_object_name,
-    --dep_on.type_desc AS referenced_object_type
-FROM
-    sys.sql_expression_dependencies sed
-JOIN
-    sys.objects o ON sed.referencing_id = o.object_id
-JOIN
-    sys.objects dep_on ON sed.referenced_id = dep_on.object_id
-WHERE
-    OBJECT_NAME(referenced_id) = @tblName
-ORDER BY
-	referencing_object_type
-	, referencing_object_name
-;
-
-
+--SUMMARY
 
 
