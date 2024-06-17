e6638be8a41073fed4a0aa1a215051c01c6da465
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-06-17 09:03:54 -0600

    D

--- redbone_analysis.sql
+++ redbone_analysis.sql
@@ -1,9 +1,10 @@
-
 --SUMMARY
 	--LOAD
 		SELECT
-			o.type_desc AS 'types referencing LOAD',
-			COUNT(*) as 'type count'
+			OBJECT_NAME(referenced_id) AS 'referencED object'
+			,'<---------' AS ' '
+			,o.type_desc AS 'referencING type'
+			,COUNT(*) as 'type count'
 		FROM
 			sys.sql_expression_dependencies sed
 		JOIN
@@ -11,9 +12,14 @@
 		JOIN
 			sys.objects dep_on ON sed.referenced_id = dep_on.object_id
 		WHERE
-			OBJECT_NAME(referenced_id) = 'LOAD'
+			OBJECT_NAME(referenced_id) in ('LOAD', 'LOADSTOP')
 		GROUP BY
-			o.type_desc
+			OBJECT_NAME(referenced_id)
+			,o.type_desc
+		ORDER BY
+			OBJECT_NAME(referenced_id)
+			,COUNT(*) desc
+			,o.type_desc
 		;
 
 
@@ -21,10 +27,11 @@
 	--DETAIL
 		--LOAD
 			SELECT
-				OBJECT_NAME(referenced_id) AS 'Referenced Name'
-				,dep_on.type_desc AS 'Referenced Type'
-				,OBJECT_NAME(referencing_id) AS referencing_object_name
-				,o.type_desc AS referencing_object_type
+				OBJECT_NAME(referenced_id) AS 'referencED object'
+				--,dep_on.type_desc AS 'referencED type'
+				,'<---------' AS ' '
+				,OBJECT_NAME(referencing_id) AS 'referencING object'
+				,o.type_desc AS 'referencING type'
 			FROM
 				sys.sql_expression_dependencies sed
 			JOIN
@@ -35,9 +42,19 @@
 				OBJECT_NAME(referenced_id) in ('LOAD', 'LOADSTOP')
 			ORDER BY
 				OBJECT_NAME(referenced_id)
-				,referencing_object_type
-				, referencing_object_name
+				--,dep_on.type_desc
+				,OBJECT_NAME(referencing_id)
 			;
+
+
+
+
+
+
+
+
+
+
 
 	/*
 
