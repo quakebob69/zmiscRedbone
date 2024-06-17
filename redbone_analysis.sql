be784f3c150305e8106864c1ab56a126e6e740b2
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-06-17 08:09:00 -0600

    d

--- redbone_analysis.sql
+++ redbone_analysis.sql
@@ -1,13 +1,12 @@
 declare @tblName varchar(100)
 
---DETAIL
+
+--SUMMARY
 	set @tblName = 'LOAD'
 	--LOAD
 		SELECT
-			OBJECT_NAME(referenced_id) AS referenced_object_name
-			,dep_on.type_desc AS referenced_object_type
-			,OBJECT_NAME(referencing_id) AS referencing_object_name
-			,o.type_desc AS referencing_object_type
+			o.type_desc AS 'types referencing LOAD',
+			COUNT(*) as asdf
 		FROM
 			sys.sql_expression_dependencies sed
 		JOIN
@@ -16,10 +15,32 @@ declare @tblName varchar(100)
 			sys.objects dep_on ON sed.referenced_id = dep_on.object_id
 		WHERE
 			OBJECT_NAME(referenced_id) = @tblName
-		ORDER BY
-			referencing_object_type
-			, referencing_object_name
+		GROUP BY
+			o.type_desc
 		;
+
+
+
+	--DETAIL
+		set @tblName = 'LOAD'
+		--LOAD
+			SELECT
+				OBJECT_NAME(referenced_id) AS referenced_object_name
+				,dep_on.type_desc AS referenced_object_type
+				,OBJECT_NAME(referencing_id) AS referencing_object_name
+				,o.type_desc AS referencing_object_type
+			FROM
+				sys.sql_expression_dependencies sed
+			JOIN
+				sys.objects o ON sed.referencing_id = o.object_id
+			JOIN
+				sys.objects dep_on ON sed.referenced_id = dep_on.object_id
+			WHERE
+				OBJECT_NAME(referenced_id) = @tblName
+			ORDER BY
+				referencing_object_type
+				, referencing_object_name
+			;
 
 
 	set @tblName = 'LOADSTOP'
@@ -40,8 +61,4 @@ declare @tblName varchar(100)
 		referencing_object_type
 		, referencing_object_name
 	;
-
-
---SUMMARY
-
 
