83db4b2ab02f33a695de785ca5e87b91bc2b3b35
quakebob69 <55359504+quakebob69@users.noreply.github.com>
2024-06-21 14:28:42 -0600

    d

--- SQL/aaaTimeClockEntry_current_hours_calculation.sql
+++ SQL/aaaTimeClockEntry_current_hours_calculation.sql
@@ -11,55 +11,91 @@ DECLARE @emailName NVARCHAR(25) = 'danny';
 			DECLARE @Dy VARCHAR(2)		= '21'
 
 		------------------------------------------
-		DECLARE @Hr VARCHAR(2)		= '12'
-			DECLARE @Mi VARCHAR(2)		= '46'
+		DECLARE @Hr VARCHAR(2)		= '15'
+			DECLARE @Mi VARCHAR(2)		= '10'
 
 
 --1st RECORD
-	SELECT TOP 1
-			ROUND(CAST(DATEDIFF(minute, [ClockIn], [ClockOut]) as float)/CAST(60 as float), 2) as 'Before Lunch'
-				--ROUND(CAST(DATEDIFF(minute, [ClockInAdj], [ClockOut]) as float)/CAST(60 as float), 2) as hours
-					--ROUND(CAST(DATEDIFF(minute, [ClockIn], [ClockOutAdj]) as float)/CAST(60 as float), 2) as hours
-						--ROUND(CAST(DATEDIFF(minute, [ClockInAdj], [ClockOutAdj]) as float)/CAST(60 as float), 2) as hours
-		--,'----------------------------' as ' '
-		--, *
-	FROM
-		[RedBone].[main].[TimeClockEntry]
-	WHERE
-		personid in (SELECT PersonId FROM main.person WHERE LoginId = @emailName + '@redbonetrucking.com')
-			AND
-		[ClockIn] >= CAST(GETDATE() AS DATE)
-			AND
-		[ClockIn] < DATEADD(DAY, 1, CAST(GETDATE() AS DATE))
-	ORDER BY
-		EntryId asc
+																																										/*
+																																										SELECT TOP 1
+																																												ROUND(CAST(DATEDIFF(minute, [ClockIn], [ClockOut]) as float)/CAST(60 as float), 2) as 'Before Lunch'
+																																													--ROUND(CAST(DATEDIFF(minute, [ClockInAdj], [ClockOut]) as float)/CAST(60 as float), 2) as hours
+																																														--ROUND(CAST(DATEDIFF(minute, [ClockIn], [ClockOutAdj]) as float)/CAST(60 as float), 2) as hours
+																																															--ROUND(CAST(DATEDIFF(minute, [ClockInAdj], [ClockOutAdj]) as float)/CAST(60 as float), 2) as hours
+																																											--,'----------------------------' as ' '
+																																											--, *
+																																										FROM
+																																											[RedBone].[main].[TimeClockEntry]
+																																										WHERE
+																																											personid in (SELECT PersonId FROM main.person WHERE LoginId = @emailName + '@redbonetrucking.com')
+																																												AND
+																																											[ClockIn] >= CAST(GETDATE() AS DATE)
+																																												AND
+																																											[ClockIn] < DATEADD(DAY, 1, CAST(GETDATE() AS DATE))
+																																										ORDER BY
+																																											EntryId asc
+																																										*/
 
 
 --2nd RECORD
-	SELECT TOP 1
-			ROUND(CAST(DATEDIFF(minute, [ClockIn], GETDATE()) as float)/CAST(60 as float), 2) as 'Current Hours After Lunch'
-					--ROUND(CAST(DATEDIFF(minute, [ClockInAdj], GETDATE()) as float)/CAST(60 as float), 2) as hours
-			--,'----------------------------' as ' '
-			--, *
-		FROM
-			[RedBone].[main].[TimeClockEntry]
-		WHERE
-			personid in (SELECT PersonId FROM main.person WHERE LoginId = @emailName + '@redbonetrucking.com')
-				AND
-			[ClockIn] >= CAST(GETDATE() AS DATE)
-				AND
-			[ClockIn] < DATEADD(DAY, 1, CAST(GETDATE() AS DATE))
-		ORDER BY
-			EntryId desc
+																																										/*
+																																										SELECT TOP 1
+																																												ROUND(CAST(DATEDIFF(minute, [ClockIn], GETDATE()) as float)/CAST(60 as float), 2) as 'Current Hours After Lunch'
+																																														--ROUND(CAST(DATEDIFF(minute, [ClockInAdj], GETDATE()) as float)/CAST(60 as float), 2) as hours
+																																												--,'----------------------------' as ' '
+																																												--, *
+																																											FROM
+																																												[RedBone].[main].[TimeClockEntry]
+																																											WHERE
+																																												personid in (SELECT PersonId FROM main.person WHERE LoginId = @emailName + '@redbonetrucking.com')
+																																													AND
+																																												[ClockIn] >= CAST(GETDATE() AS DATE)
+																																													AND
+																																												[ClockIn] < DATEADD(DAY, 1, CAST(GETDATE() AS DATE))
+																																											ORDER BY
+																																												EntryId desc
+																																										*/
 
 ------------------------------------------
 DECLARE @YrMoDy datetime	= @Yr + '-' + @Mo + '-' + @Dy + ' ' + @Hr + ':' + @Mi
 DECLARE @TimeToGo VARCHAR(5)	= CAST(CAST(@Hr as int) - 12 as VARCHAR) + ':' + @Mi
 ------------------------------------------
 --3rd RECORD
+																																										/*
+																																											SELECT TOP 1
+																																														--TARGET TIME
+																																														ROUND(CAST(DATEDIFF(minute, [ClockIn], @YrMoDy) as float)/CAST(60 as float), 2) as 'Needed After Lunch', @TimeToGo as 'Quiting Time'
+		
+
+																																												--ROUND(CAST(DATEDIFF(minute, [ClockIn], GETDATE()) as float)/CAST(60 as float), 2) as hours
+																																														--ROUND(CAST(DATEDIFF(minute, [ClockInAdj], GETDATE()) as float)/CAST(60 as float), 2) as hours
+																																												--,'----------------------------' as ' '
+																																												--, *
+																																											FROM
+																																												[RedBone].[main].[TimeClockEntry]
+																																											WHERE
+																																												personid in (SELECT PersonId FROM main.person WHERE LoginId = @emailName + '@redbonetrucking.com')
+																																													AND
+																																												[ClockIn] >= CAST(GETDATE() AS DATE)
+																																													AND
+																																												[ClockIn] < DATEADD(DAY, 1, CAST(GETDATE() AS DATE))
+																																											ORDER BY
+																																												EntryId desc
+																																										*/
+
+
+
+DECLARE @hourCurrent DECIMAL(38, 10)
+DECLARE @hourToReach DECIMAL(38, 10)
+
+SET @hourCurrent = 42.71
+SET @hourToReach = 45
+
+
+--4th RECORD
 	SELECT TOP 1
 				--TARGET TIME
-				ROUND(CAST(DATEDIFF(minute, [ClockIn], @YrMoDy) as float)/CAST(60 as float), 2) as 'Needed After Lunch', @TimeToGo as 'Quiting Time'
+				DATEADD(MINUTE, (60 * (@hourToReach - @hourCurrent)), [ClockIn]) as 'Quiting Time'
 		
 
 		--ROUND(CAST(DATEDIFF(minute, [ClockIn], GETDATE()) as float)/CAST(60 as float), 2) as hours
@@ -70,14 +106,20 @@ DECLARE @TimeToGo VARCHAR(5)	= CAST(CAST(@Hr as int) - 12 as VARCHAR) + ':' + @M
 		[RedBone].[main].[TimeClockEntry]
 	WHERE
 		personid in (SELECT PersonId FROM main.person WHERE LoginId = @emailName + '@redbonetrucking.com')
-			AND
-		[ClockIn] >= CAST(GETDATE() AS DATE)
-			AND
-		[ClockIn] < DATEADD(DAY, 1, CAST(GETDATE() AS DATE))
 	ORDER BY
 		EntryId desc
 
-SELECT * FROM [RedBone].[main].[TimeClockEntry] WHERE personid in (SELECT PersonId FROM main.person WHERE LoginId = @emailName + '@redbonetrucking.com') AND [ClockIn] >= CAST(GETDATE() AS DATE) AND [ClockIn] < DATEADD(DAY, 1, CAST(GETDATE() AS DATE)) order by EntryId desc
+
+
+
+
+
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------------SELECT * FROM [RedBone].[main].[TimeClockEntry] WHERE personid in (SELECT PersonId FROM main.person WHERE LoginId = @emailName + '@redbonetrucking.com') AND [ClockIn] >= CAST(GETDATE() AS DATE) AND [ClockIn] < DATEADD(DAY, 1, CAST(GETDATE() AS DATE)) order by EntryId desc
 
 
 --SELECT * FROM [RedBone].[main].[TimeClockEntry] where personid = 2775 order by entryid desc
@@ -97,16 +139,16 @@ SELECT top 5 * FROM [RedBone].[main].[TimeClockEntry] WHERE personid in (SELECT 
 -------------------------------------------------------------------------------------------------------------------------------------------------
 	--Set dates manually
 		--IN
-			--DECLARE @EntryId__IN INTEGER = 27462
-			--DECLARE @Clocked__IN NVARCHAR(25)		= '2024-06-19 06:57:04.940';
+			--DECLARE @EntryId__IN INTEGER = 27511
+			--DECLARE @Clocked__IN NVARCHAR(25)		= '2024-06-21 12:49:04.940';
 			--UPDATE [RedBone].[main].[TimeClockEntry] SET ClockIn = @Clocked__IN WHERE EntryId = @EntryId__IN
 
 		
 
 		--OUT
-			DECLARE @EntryId__OUT INTEGER = 27420
-			DECLARE @Clocked__OUT NVARCHAR(25)		= '2024-06-20 17:04:53.821';
-			UPDATE [RedBone].[main].[TimeClockEntry] SET ClockOut = @Clocked__OUT WHERE EntryId = @EntryId__OUT
+			--DECLARE @EntryId__OUT INTEGER = 27462
+			--DECLARE @Clocked__OUT NVARCHAR(25)		= '2024-06-21 11:28:12.053';
+			--UPDATE [RedBone].[main].[TimeClockEntry] SET ClockOut = @Clocked__OUT WHERE EntryId = @EntryId__OUT
 -------------------------------------------------------------------------------------------------------------------------------------------------
 SELECT top 5 * FROM [RedBone].[main].[TimeClockEntry] WHERE personid in (SELECT PersonId FROM main.person WHERE LoginId = @emailName2 + '@redbonetrucking.com') order by EntryId desc
 -------------------------------------------------------------------------------------------------------------------------------------------------
