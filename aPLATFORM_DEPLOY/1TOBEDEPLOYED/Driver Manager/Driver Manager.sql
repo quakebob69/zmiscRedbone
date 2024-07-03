64d28710a4fd32160c3dd4cb3ae083aa8c305166
Daniel Call <danny@redbonetrucking.com>
2024-07-03 09:52:30 -0600

    D

---
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/Driver Manager/Driver Manager.sql
@@ -0,0 +1,34 @@
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dispatch].[DispatchFleet]') AND type in (N'U'))
+DROP TABLE [dispatch].[DispatchFleet]
+GO
+
+
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dispatch].[DispatchFleet](
+	[DispatchFleetId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](128) NULL,
+	[PersonId] [int] NOT NULL
+ CONSTRAINT [PK_DispatchFleet] PRIMARY KEY CLUSTERED 
+(
+	[DispatchFleetId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [UQ_DispatchFleet] UNIQUE NONCLUSTERED 
+(
+	[Name]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dispatch].[DispatchFleet]  WITH CHECK ADD  CONSTRAINT [FK_DispatchFleet_Person] FOREIGN KEY([PersonId])
+REFERENCES [main].[Person] ([PersonId])
+GO
+ALTER TABLE [dispatch].[DispatchFleet] CHECK CONSTRAINT [FK_DispatchFleet_Person]
+GO
+
+
