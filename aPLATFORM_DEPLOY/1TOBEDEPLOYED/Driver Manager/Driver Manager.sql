b2221387426ac17e543eef66f5bfc9516dd433aa
Daniel Call <danny@redbonetrucking.com>
2024-07-03 10:15:19 -0600

    d

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/Driver Manager/Driver Manager.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/Driver Manager/Driver Manager.sql
@@ -39,3 +39,16 @@ INSERT INTO [dispatch].[DispatchFleet] ([Name] ,[PersonId]) VALUES  ('Whirling D
 GO
 
 
+
+
+
+ALTER TABLE [main].[Person]
+ADD [PayrollOTRBenefitsStatusId] [int] NULL
+GO
+
+ALTER TABLE [main].[Person]  WITH CHECK ADD  CONSTRAINT [FK_Person_PayrollOTRBenefitsStatus] FOREIGN KEY([PayrollOTRBenefitsStatusId])
+REFERENCES [payroll].[PayrollOTRBenefitsStatus] ([PayrollOTRBenefitsStatusId])
+GO
+ALTER TABLE [main].[Person] CHECK CONSTRAINT [FK_Person_PayrollOTRBenefitsStatus]
+GO
+
