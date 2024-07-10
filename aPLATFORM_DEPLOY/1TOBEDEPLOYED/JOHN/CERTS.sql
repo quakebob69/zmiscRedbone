2657f96b9cad83be8a3d26e1ed4058d285fb1899
Daniel Call <danny@redbonetrucking.com>
2024-07-10 08:32:30 -0600

    d

---
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/JOHN/CERTS.sql
@@ -0,0 +1,17 @@
+select * from main.CertificationPersonMapping where filename is not null
+
+
+select * from main.CertificationPersonMapping where PersonId = (select top 1 PersonId from main.person where LastName = 'BRAVO')
+select * from main.CertificationPersonMapping where PersonId = (select top 1 PersonId from main.person where FIRSTNAME = 'DANIEL' AND LastName = 'CALL')
+
+UPDATE main.CertificationPersonMapping SET FileName = '07-09-2024 Medical Card.pdf' WHERE CertificationPersonMappingID = 7220
+UPDATE main.CertificationPersonMapping SET FileName = null WHERE CertificationPersonMappingID = 7681
+
+
+
+
+select * from main.CertificationPersonMapping where filename is not null
+ORDER BY PERSONID
+
+
+--where PersonId = (select top 1 PersonId from main.person where LastName = 'BRAVO')
