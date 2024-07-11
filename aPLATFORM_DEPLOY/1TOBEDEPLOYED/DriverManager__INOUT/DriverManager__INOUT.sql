8dfb08844424ee4b17cafdd4548985e789a26623
Daniel Call <danny@redbonetrucking.com>
2024-07-11 09:43:16 -0600

    y

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT.sql
@@ -230,48 +230,50 @@
 
 
 
-	------------
-		CREATE VIEW [dispatch].[vInOut] AS
-			/*
-				select top 100 loadid from dispatch.load order by loadid desc
-			*/
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+																																				DROP VIEW IF EXISTS [dispatch].[vInOut]
+																																				GO
 
+																																				CREATE VIEW [dispatch].[vInOut] AS
+																																					/*
+																																						select top 100 loadid from dispatch.load order by loadid desc
+																																					*/
 
-
-			select top 1000
-				l.dispatchFleetManagerid
-				--,fm.name
-				--,p.FirstName + ' ' + p.LastName as FullName
-				,l.loadid
-				,ls.LoadStopTypeId
-				,ls.ArrivalDateTime
-				,ls.DepartureDateTime
-			from
-				dispatch.load l
-				join dispatch.loadStop ls on l.loadid = ls.loadid
-				--join dispatch.DispatchFleetManager fm on l.dispatchFleetManagerid = fm.DispatchFleetManagerId
-				--join main.Person p on p.PersonId = fm.PersonId
-			where
-
-
-
-
-							l.loadid > 63741
-							and
-
-				(ls.LoadStopTypeId = 1 or ls.LoadStopTypeId = 3)
-				and
-				(ls.ArrivalDateTime is null OR ls.DepartureDateTime is null)
+																																					select top 1000
+																																						l.dispatchFleetManagerid
+																																						--,fm.name
+																																						--,p.FirstName + ' ' + p.LastName as FullName
+																																						,l.loadid
+																																						,ls.LoadStopTypeId
+																																						,ls.ArrivalDateTime
+																																						,ls.DepartureDateTime
+																																					from
+																																						dispatch.load l
+																																						join dispatch.loadStop ls on l.loadid = ls.loadid
+																																						--join dispatch.DispatchFleetManager fm on l.dispatchFleetManagerid = fm.DispatchFleetManagerId
+																																						--join main.Person p on p.PersonId = fm.PersonId
+																																					where
 
 
 
 
-			order by
-			l.dispatchFleetManagerid,
-			loadid desc 
+
+																																									l.loadid > 63741
+																																									and
 
 
-		GO
+
+
+
+																																						(ls.LoadStopTypeId = 1 or ls.LoadStopTypeId = 3)
+																																						and
+																																						(ls.ArrivalDateTime is null OR ls.DepartureDateTime is null)
+
+																																					order by
+																																					l.dispatchFleetManagerid,
+																																					loadid desc 
+																																				GO
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
