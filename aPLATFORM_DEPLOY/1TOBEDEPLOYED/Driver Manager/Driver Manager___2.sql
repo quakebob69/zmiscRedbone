644dd1dcb9dec827aa6dd35229ec5fb1b9dbc532
Daniel Call <danny@redbonetrucking.com>
2024-07-11 09:00:14 -0600

    d

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/Driver Manager/Driver Manager___2.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/Driver Manager/Driver Manager___2.sql
@@ -1,25 +1,3 @@
-	select top 100
-		l.dispatchFleetManagerid
-		--,fm.name
-		--,p.FirstName + ' ' + p.LastName as FullName
-		,l.loadid
-		,ls.LoadStopTypeId
-		,ls.ArrivalDateTime
-		,ls.DepartureDateTime
-	from
-		dispatch.load l
-		join dispatch.loadStop ls on l.loadid = ls.loadid
-		--join dispatch.DispatchFleetManager fm on l.dispatchFleetManagerid = fm.DispatchFleetManagerId
-		--join main.Person p on p.PersonId = fm.PersonId
-	where
-		--l.loadid > asdf
-		(ls.LoadStopTypeId = 1 or ls.LoadStopTypeId = 3)
-		and
-		(ls.ArrivalDateTime is null OR ls.DepartureDateTime is null)
-	order by loadid desc
-
-
-
 /*
 	select * from main.PersonTypes
 
--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/Driver Manager/Driver Manager___3.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/Driver Manager/Driver Manager___3.sql
@@ -1,14 +1,30 @@
-DROP VIEW IF EXISTS [payroll].[vPayrollOTRCurrentPayPeriod]
+DROP VIEW IF EXISTS [dispatch].[vInOut]
 GO
 
 
-CREATE VIEW [payroll].[vPayrollOTRCurrentPayPeriod] AS
+CREATE VIEW [dispatch].[vInOut] AS
 
-	SELECT
-		*
-	FROM
-		[payroll].PayrollOTRPayPeriod
-	WHERE
-		IsActive = 1
+	select top 100
+		l.dispatchFleetManagerid
+		--,fm.name
+		--,p.FirstName + ' ' + p.LastName as FullName
+		,l.loadid
+		,ls.LoadStopTypeId
+		,ls.ArrivalDateTime
+		,ls.DepartureDateTime
+	from
+		dispatch.load l
+		join dispatch.loadStop ls on l.loadid = ls.loadid
+		--join dispatch.DispatchFleetManager fm on l.dispatchFleetManagerid = fm.DispatchFleetManagerId
+		--join main.Person p on p.PersonId = fm.PersonId
+	where
+		--l.loadid > asdf
+		(ls.LoadStopTypeId = 1 or ls.LoadStopTypeId = 3)
+		and
+		(ls.ArrivalDateTime is null OR ls.DepartureDateTime is null)
+	order by loadid desc
+
+
+
 
 GO
