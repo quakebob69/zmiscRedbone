6b09c3e9d4c5a65d3ca8ab379d6ec7f4a5641326
Daniel Call <danny@redbonetrucking.com>
2024-07-11 09:24:54 -0600

    d

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT.sql
@@ -1,5 +1,8 @@
 --DROP STUFF
 ------------------------------------------------
+	DROP VIEW IF EXISTS [dispatch].[vInOut]
+	GO
+
 	DROP PROCEDURE IF EXISTS [dbo].[sp_Load_SaveFleetManager]
 	GO
 
@@ -219,6 +222,51 @@
 			DispatchFleetManagerId = @DispatchFleetManagerId
 		WHERE        
 			(LoadId = @LoadId)
+		GO
+
+
+
+
+
+
+
+	------------
+		CREATE VIEW [dispatch].[vInOut] AS
+			/*
+				select top 100 loadid from dispatch.load order by loadid desc
+			*/
+
+
+
+			select top 1000
+				l.dispatchFleetManagerid
+				--,fm.name
+				--,p.FirstName + ' ' + p.LastName as FullName
+				,l.loadid
+				,ls.LoadStopTypeId
+				,ls.ArrivalDateTime
+				,ls.DepartureDateTime
+			from
+				dispatch.load l
+				join dispatch.loadStop ls on l.loadid = ls.loadid
+				--join dispatch.DispatchFleetManager fm on l.dispatchFleetManagerid = fm.DispatchFleetManagerId
+				--join main.Person p on p.PersonId = fm.PersonId
+			where
+
+
+
+				l.loadid > 63741
+				and
+
+
+
+
+				(ls.LoadStopTypeId = 1 or ls.LoadStopTypeId = 3)
+				and
+				(ls.ArrivalDateTime is null OR ls.DepartureDateTime is null)
+			order by loadid desc
+
+
 		GO
 
 
