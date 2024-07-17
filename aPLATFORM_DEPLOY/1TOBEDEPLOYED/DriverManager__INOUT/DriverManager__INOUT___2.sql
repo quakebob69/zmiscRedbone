0dd801759bfb3174b88914d6cbcd64ffaa907219
Daniel Call <danny@redbonetrucking.com>
2024-07-17 07:25:20 -0600

    f

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT___2.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT___2.sql
@@ -10,3 +10,29 @@
 	where
 	PersonTypeid = 19
 */
+
+
+/*
+	--62602
+
+	1642	260
+	1591	261
+	1648	262
+	1649	263
+	1650	264
+	1651	265
+	1652	266
+	1653	267
+	1654	268
+	1655	269
+
+
+
+	207 = spare
+	272 = spare
+*/
+
+
+select * from equipment.PUnit where  DispatchFleetManagerId is not null order by DispatchFleetManagerId;
+select * from equipment.PUnit where Unit_ID in ('207', '272');
+
