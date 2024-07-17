4c92da2b183d03d70a12543f73bc34e25c879c1f
Daniel Call <danny@redbonetrucking.com>
2024-07-17 10:24:50 -0600

    d

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT___2.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT___2.sql
@@ -1,40 +1,3 @@
-/*
-	select * from main.PersonTypes
-
-
-	select
-		*
-	from
-		main.PersonTypeMapping pm
-		join main.Person p on p.PersonId = pm.PersonId
-	where
-	PersonTypeid = 19
-*/
-
-
-
-/*
-	--62602
-
-	1642	260
-	1591	261
-	1648	262
-	1649	263
-	1650	264
-	1651	265
-	1652	266
-	1653	267
-	1654	268
-	1655	269
-
-
-
-	207 = spare
-	272 = spare
-*/
-
-
-
 --  fleetManagers/truck/drivers
 	select e.DispatchFleetManagerId, e.Unit_ID, p.FirstName + ' ' + p.LastName as FullName from
 		main.Driver d
@@ -46,11 +9,6 @@
 			where pu.DispatchFleetManagerId is not null
 		)
 	ORDER BY TRY_CAST(e.Unit_ID AS INT) desc
-
-
-
-
---select punitid, Unit_ID from equipment.PUnit where Unit_ID in ('207', '272');
 
 
 
@@ -98,11 +56,39 @@
 
 
 
+/*
+	select * from main.PersonTypes
+
+
+	select
+		*
+	from
+		main.PersonTypeMapping pm
+		join main.Person p on p.PersonId = pm.PersonId
+	where
+	PersonTypeid = 19
+*/
+
+
+/*
+	--62602
+
+	1642	260
+	1591	261
+	1648	262
+	1649	263
+	1650	264
+	1651	265
+	1652	266
+	1653	267
+	1654	268
+	1655	269
 
 
 
-
-
+	207 = spare
+	272 = spare
+*/
 
 
 /*
@@ -111,5 +97,4 @@
       ,[Enabled]
 	FROM [RedBone].[main].[GroupType]
 */
-
 
