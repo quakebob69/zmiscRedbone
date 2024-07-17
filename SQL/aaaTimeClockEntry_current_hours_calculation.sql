189dade6c82b9781c6fa53204f3457dfdf4c0935
Daniel Call <danny@redbonetrucking.com>
2024-07-17 16:25:50 -0600

    d

--- SQL/aaaTimeClockEntry_current_hours_calculation.sql
+++ SQL/aaaTimeClockEntry_current_hours_calculation.sql
@@ -18,9 +18,9 @@ DECLARE @emailName NVARCHAR(25) = 'danny';
 		
 
 																						--OUT
-																							--DECLARE @EntryId__OUT INTEGER = 28142
-																							--DECLARE @Clocked__OUT NVARCHAR(25)		= '2024-07-15 14:57:42.627';
-																							--UPDATE [RedBone].[main].[TimeClockEntry] SET ClockOut = @Clocked__OUT WHERE EntryId = @EntryId__OUT;
+																							DECLARE @EntryId__OUT INTEGER = 28142
+																							DECLARE @Clocked__OUT NVARCHAR(25)		= '2024-07-15 14:55:51.843';
+																							UPDATE [RedBone].[main].[TimeClockEntry] SET ClockOut = @Clocked__OUT WHERE EntryId = @EntryId__OUT;
 																					--------------------------------------------------------------------------------------------------------------
 
 		-------------------------------------------------------------------------------------------------------------------------------------------------
