c732080430a505862fec9d7c6dd4666f052d12a4
Daniel Call <danny@redbonetrucking.com>
2024-07-24 09:46:36 -0600

    d

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT.sql
@@ -458,7 +458,26 @@
 		AS
 			DROP TABLE IF EXISTS #TEMP_PUnit_Active
 			CREATE TABLE #TEMP_PUnit_Active(
-				PUnitId int NULL
+			PUnitId int NULL
+			,Unit_ID [varchar](50) NULL
+			,VIN [varchar](50) NULL
+			,PUnitMakeId int NULL
+			,PUnitMakeModelId int NULL
+			,Mod_Year int NULL
+			,Notes [text] NULL
+			,InService [datetime] NULL
+			,ActiveInd bit NULL
+			,PlateNumber varchar(50) NULL
+			,TireSize varchar(20) NULL
+			,Company varchar(50) NULL
+			,Division varchar(50) NULL
+			,[Group] varchar(50) NULL
+			, Client varchar(100) NULL
+			,[Location] = lt.[Location]
+			,CAST(isnull(a.Indicator,0) as bit) as Indicator
+			,CAST(isnull(a1.Indicatoryellow,0) as bit) as Indicatoryellow
+			,p.EquipTypeId int NULL
+
 			)
 			
 			INSERT INTO #TEMP_PUnit_Active
