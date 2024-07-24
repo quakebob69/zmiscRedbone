80be0c5395b21744f9f4b6263f150526fbc17bea
Daniel Call <danny@redbonetrucking.com>
2024-07-24 12:43:01 -0600

    Update DriverManager__INOUT.sql

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT.sql
@@ -446,88 +446,6 @@
 
 
 
-	------------
-	/****** Object:  StoredProcedure [dbo].[sp_DriverManagerUnassignedTrucks]    Script Date: 7/10/2024 7:11:14 PM ******/
-		SET ANSI_NULLS ON
-		GO
-
-		SET QUOTED_IDENTIFIER ON
-		GO
-
-		CREATE procedure [dbo].[sp_DriverManagerUnassignedTrucks]
-		AS
-		
-			DROP TABLE IF EXISTS #TEMP_PUnit_Active
-			CREATE TABLE #TEMP_PUnit_Active(
-			PUnitId int NULL
-			,Unit_ID [varchar](50) NULL
-			,VIN [varchar](50) NULL
-			,PUnitMakeId int NULL
-			,PUnitMakeModelId int NULL
-			,Mod_Year int NULL
-			,Notes [text] NULL
-			,InService [datetime] NULL
-			,ActiveInd bit NULL
-			,PlateNumber varchar(50) NULL
-			,TireSize varchar(20) NULL
-			,Company varchar(50) NULL
-			,Division varchar(50) NULL
-			,[Group] varchar(50) NULL
-			, Client varchar(100) NULL
-			,[Location] varchar(50) NULL
-			,Indicator bit NULL
-			,Indicatoryellow bit NULL
-			,EquipTypeId int NULL
-			)
-			
-			INSERT INTO #TEMP_PUnit_Active
-			EXEC [dbo].[sp_Equipment_PUnit_For_Grid] 0
-
-			DROP TABLE IF EXISTS #TEMP_PUnit_AssignedToDriverMans
-			CREATE TABLE #TEMP_PUnit_AssignedToDriverMans(
-			PUnitId int NULL
-			)			
-			INSERT INTO #TEMP_PUnit_AssignedToDriverMans (PUnitId)
-			SELECT PUnitId FROM dispatch.vFleetManagerDriver where DispatchFleetManagerId = 1
-			UNION ALL
-			SELECT PUnitId FROM dispatch.vFleetManagerDriver where DispatchFleetManagerId = 2
-			UNION ALL
-			SELECT PUnitId FROM dispatch.vFleetManagerDriver where DispatchFleetManagerId = 3
-
-			/*
-			select
-			(select count(distinct allTrcks.PUnitId) from #TEMP_PUnit_Active allTrcks)			
-			-
-			(select count(distinct asgnedTrucks.PUnitId) from #TEMP_PUnit_AssignedToDriverMans asgnedTrucks)
-
-			select count(distinct allTrcks.PUnitId) from
-			#TEMP_PUnit_Active allTrcks
-			left join #TEMP_PUnit_AssignedToDriverMans asgnedTrucks on allTrcks.PUnitId = asgnedTrucks.PUnitId
-			where asgnedTrucks.PUnitId IS NULL
-
-			select count(distinct allTrcks.PUnitId) from #TEMP_PUnit_Active allTrcks
-			
-			select count(distinct asgnedTrucks.PUnitId) from #TEMP_PUnit_AssignedToDriverMans asgnedTrucks
-			
-			select count(distinct allTrcks.PUnitId) from
-			#TEMP_PUnit_Active allTrcks
-			left join #TEMP_PUnit_AssignedToDriverMans asgnedTrucks on allTrcks.PUnitId = asgnedTrucks.PUnitId
-			where asgnedTrucks.PUnitId IS NULL
-			*/
-
-			select distinct allTrcks.PUnitId from
-			#TEMP_PUnit_Active allTrcks
-			left join #TEMP_PUnit_AssignedToDriverMans asgnedTrucks on allTrcks.PUnitId = asgnedTrucks.PUnitId
-			where asgnedTrucks.PUnitId IS NULL
-		GO
-
-
-
-		
-
-
-		exec [dbo].[sp_DriverManagerUnassignedTrucks]
-
 
 
 
@@ -632,6 +550,100 @@
 --select * from [dispatch].[vFleetManagerDriver] where DispatchFleetManagerId = 1
 --select * from [dispatch].[vFleetManagerDriver] where DispatchFleetManagerId = 2
 --select * from [dispatch].[vFleetManagerDriver] where DispatchFleetManagerId = 3
+
+
+
+
+
+
+
+	------------
+	/****** Object:  StoredProcedure [dbo].[sp_DriverManagerUnassignedTrucks]    Script Date: 7/10/2024 7:11:14 PM ******/
+		SET ANSI_NULLS ON
+		GO
+
+		SET QUOTED_IDENTIFIER ON
+		GO
+
+		CREATE procedure [dbo].[sp_DriverManagerUnassignedTrucks]
+		AS
+		
+			DROP TABLE IF EXISTS #TEMP_PUnit_Active
+			CREATE TABLE #TEMP_PUnit_Active(
+			PUnitId int NULL
+			,Unit_ID [varchar](50) NULL
+			,VIN [varchar](50) NULL
+			,PUnitMakeId int NULL
+			,PUnitMakeModelId int NULL
+			,Mod_Year int NULL
+			,Notes [text] NULL
+			,InService [datetime] NULL
+			,ActiveInd bit NULL
+			,PlateNumber varchar(50) NULL
+			,TireSize varchar(20) NULL
+			,Company varchar(50) NULL
+			,Division varchar(50) NULL
+			,[Group] varchar(50) NULL
+			, Client varchar(100) NULL
+			,[Location] varchar(50) NULL
+			,Indicator bit NULL
+			,Indicatoryellow bit NULL
+			,EquipTypeId int NULL
+			)
+			
+			INSERT INTO #TEMP_PUnit_Active
+			EXEC [dbo].[sp_Equipment_PUnit_For_Grid] 0
+
+			DROP TABLE IF EXISTS #TEMP_PUnit_AssignedToDriverMans
+			CREATE TABLE #TEMP_PUnit_AssignedToDriverMans(
+			PUnitId int NULL
+			)			
+			INSERT INTO #TEMP_PUnit_AssignedToDriverMans (PUnitId)
+			SELECT PUnitId FROM dispatch.vFleetManagerDriver where DispatchFleetManagerId = 1
+			UNION ALL
+			SELECT PUnitId FROM dispatch.vFleetManagerDriver where DispatchFleetManagerId = 2
+			UNION ALL
+			SELECT PUnitId FROM dispatch.vFleetManagerDriver where DispatchFleetManagerId = 3
+
+			/*
+			select
+			(select count(distinct allTrcks.PUnitId) from #TEMP_PUnit_Active allTrcks)			
+			-
+			(select count(distinct asgnedTrucks.PUnitId) from #TEMP_PUnit_AssignedToDriverMans asgnedTrucks)
+
+			select count(distinct allTrcks.PUnitId) from
+			#TEMP_PUnit_Active allTrcks
+			left join #TEMP_PUnit_AssignedToDriverMans asgnedTrucks on allTrcks.PUnitId = asgnedTrucks.PUnitId
+			where asgnedTrucks.PUnitId IS NULL
+
+			select count(distinct allTrcks.PUnitId) from #TEMP_PUnit_Active allTrcks
+			
+			select count(distinct asgnedTrucks.PUnitId) from #TEMP_PUnit_AssignedToDriverMans asgnedTrucks
+			
+			select count(distinct allTrcks.PUnitId) from
+			#TEMP_PUnit_Active allTrcks
+			left join #TEMP_PUnit_AssignedToDriverMans asgnedTrucks on allTrcks.PUnitId = asgnedTrucks.PUnitId
+			where asgnedTrucks.PUnitId IS NULL
+			*/
+
+			select distinct allTrcks.PUnitId from
+			#TEMP_PUnit_Active allTrcks
+			left join #TEMP_PUnit_AssignedToDriverMans asgnedTrucks on allTrcks.PUnitId = asgnedTrucks.PUnitId
+			where asgnedTrucks.PUnitId IS NULL
+		GO
+
+
+
+		
+
+
+		exec [dbo].[sp_DriverManagerUnassignedTrucks]
+
+
+
+
+
+
 
 
 
