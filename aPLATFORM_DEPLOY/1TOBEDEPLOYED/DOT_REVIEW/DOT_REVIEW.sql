9431dda4a67d55554911cd38de7adba003358590
Daniel Call <danny@redbonetrucking.com>
2024-07-25 11:19:24 -0600

    Update DOT_REVIEW.sql

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/DOT_REVIEW/DOT_REVIEW.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/DOT_REVIEW/DOT_REVIEW.sql
@@ -123,7 +123,7 @@ select top 1
 	
 		,@HireRehireDate as 'Date of Hire (MM/DD/YYYY)'
 		
-		,''
+		,' - '
 	
 /*	,'-------------------------' as '-------------------------'
 	
@@ -231,4 +231,58 @@ where
 	StatusChangeDate >= DATEADD(day, -366, GETDATE())
 order by
 	p.PersonId, StatusChangeDate
+	*/
+
+
+
+	/*
+
+	
+select
+	p.FirstName as 'Driver First Name'
+	,p.LastName as 'Driver Last Name'
+	
+	,Convert(VARCHAR, p.Birthday, 101) as 'Date of Birth (MM/DD/YYYY)'
+
+		,'asdf' as 'License State'
+		,'asdf' as 'License #'
+		,'asdf' as 'CDL (Y/N)'
+	
+		,'asdf' as 'Phone Number'
+	
+		,'asdf' as 'Date of Hire (MM/DD/YYYY)'
+		
+		,'asdf' as 'Date of Termination (MM/DD/YYYY)'
+	
+	,'-------------------------' as '-------------------------'
+	
+	,pt.PersonType-- as 'Person Type'
+	,phst.Description as 'HiringS tatus Type'
+	,Convert(VARCHAR, sh.StatusChangeDate, 101) as 'Status Change Date'
+
+	,'-------------------------' as '-------------------------'
+
+	--,'asdf' as 'asdf'
+	
+	,*
+
+	
+from
+	main.PersonHiringStatusHistory sh
+	join main.PersonHiringStatusType phst on phst.PersonHiringStatusTypeId = sh.PersonHiringStatusTypeId
+	join main.Person p on p.PersonId = sh.PersonId
+	join main.PersonTypeMapping ptm on p.PersonId = ptm.PersonId
+	join main.PersonTypes pt on pt.PersonTypeId = ptm.PersonTypeId
+where
+	ptm.PersonTypeId = 4
+		
+		--and cpm
+		--PHONE NUMBE
+
+	and
+	StatusChangeDate >= DATEADD(day, -366, GETDATE())
+order by
+	p.Personid
+
+
 	*/
