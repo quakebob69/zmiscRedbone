11aec17e70fab287556a2b98b2e1c91bc587ffd8
Daniel Call <danny@redbonetrucking.com>
2024-07-25 08:48:58 -0600

    Update DOT_REVIEW.sql

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/DOT_REVIEW/DOT_REVIEW.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/DOT_REVIEW/DOT_REVIEW.sql
@@ -1,29 +1,39 @@
 
 select
-	p.FirstName as 'Driver First Name',
-	p.LastName as 'Driver Last Name',
+	p.FirstName as 'Driver First Name'
+	,p.LastName as 'Driver Last Name'
 	
-	'Date of Birth (MM/DD/YYY)' as 'Date of Birth (MM/DD/YYY)',
+	,'Date of Birth (MM/DD/YYY)' as 'Date of Birth (MM/DD/YYY)'
 	
-	'License State' as 'License State',
+	,'License State' as 'License State'
 	
-	'CDL (Y/N)' as 'CDL (Y/N)', 'Phone Number' as 'Phone Number',
+	,'CDL (Y/N)' as 'CDL (Y/N)', 'Phone Number' as 'Phone Number'
 	
-	'Date of Hire (MM/DD/YYY)' as 'Date of Hire (MM/DD/YYY)', 
+	,'Date of Hire (MM/DD/YYY)' as 'Date of Hire (MM/DD/YYY)'
 	
-	'----------------------------------------',
+	,'----------------------------------------'
 	
-	'Date of Termination (MM/DD/YYY)' as 'Date of Termination (MM/DD/YYY)',    '----------------------------------------',
+	,'Date of Termination (MM/DD/YYY)' as 'Date of Termination (MM/DD/YYY)'
 	
-	'asdf' as 'asdf',
+	,'----------------------------------------'
 	
-	*, 
+	,pt.PersonType-- as 'Person Type'
+
+	,'asdf' as 'asdf'
 	
-	Convert(DATE, sh.StatusChangeDate, 108) as Status_Change_Date
+	,*
+
+	
+	,Convert(DATE, sh.StatusChangeDate, 108) as Status_Change_Date
 from
-	main.PersonHiringStatusHistory sh
+	main.CertificationPersonMapping cpm
+	join main.PersonHiringStatusHistory sh on cpm.PersonId = sh.Personid
 	join main.Person p on p.PersonId = sh.PersonId
+	join main.PersonTypeMapping ptm on p.PersonId = ptm.PersonId
+	join main.PersonTypes pt on pt.PersonTypeId = ptm.PersonTypeId
 where
+	ptm.PersonTypeId = 4-- = 
+	and
 	StatusChangeDate >= DATEADD(day, -366, GETDATE())
 order by
 	p.PersonId, StatusChangeDate
