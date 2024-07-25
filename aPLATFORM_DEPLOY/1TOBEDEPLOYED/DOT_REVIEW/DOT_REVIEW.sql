e6aaf8e8d18517bf551976f9a3aa6a69f3982aba
Daniel Call <danny@redbonetrucking.com>
2024-07-25 11:01:51 -0600

    d

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/DOT_REVIEW/DOT_REVIEW.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/DOT_REVIEW/DOT_REVIEW.sql
@@ -74,16 +74,11 @@ SELECT PersonId FROM @IDList;
 
 			IF @LAST_HiringStatusType = 1 OR @LAST_HiringStatusType = 4
 			BEGIN
-				SELECT @LAST_HiringStatusType;
-			END
-
-
-			-- Insert the result into the temporary table
 				INSERT INTO @FINAL_LIST
 				(
 					PersonId
-					,FirstName
-					,LastName
+					,FirstName)
+					/*,LastName
 					,Birthday
 					,LicenseNumb
 					,LicenseState
@@ -103,8 +98,62 @@ SELECT PersonId FROM @IDList;
 					,'CDL_Y_N'
 					,'PhoneNumber'
 					,'DateHire'
-					,'DateTermination'
-				)
+					,''
+				)*/
+select
+	@PERSONID,
+	p.FirstName as 'Driver First Name'
+/*	,p.LastName as 'Driver Last Name'
+	
+	,Convert(VARCHAR, p.Birthday, 101) as 'Date of Birth (MM/DD/YYYY)'
+
+		,'asdf' as 'License State'
+		,'asdf' as 'License #'
+		,'asdf' as 'CDL (Y/N)'
+	
+		,'asdf' as 'Phone Number'
+	
+		,'asdf' as 'Date of Hire (MM/DD/YYYY)'
+		
+		,'asdf' as 'Date of Termination (MM/DD/YYYY)'
+	
+	,'-------------------------' as '-------------------------'
+	
+	,pt.PersonType-- as 'Person Type'
+	,phst.Description as 'HiringS tatus Type'
+	,Convert(VARCHAR, sh.StatusChangeDate, 101) as 'Status Change Date'
+
+	,'-------------------------' as '-------------------------'
+
+	--,'asdf' as 'asdf'
+	
+	,*
+	*/
+	
+from
+	main.CertificationPersonMapping cpm
+	join main.PersonHiringStatusHistory sh on cpm.PersonId = sh.Personid
+	join main.PersonHiringStatusType phst on phst.PersonHiringStatusTypeId = sh.PersonHiringStatusTypeId
+	join main.Person p on p.PersonId = sh.PersonId
+	join main.PersonTypeMapping ptm on p.PersonId = ptm.PersonId
+	join main.PersonTypes pt on pt.PersonTypeId = ptm.PersonTypeId
+where
+	ptm.PersonTypeId = 4
+		
+		--and cpm
+		--PHONE NUMBE
+
+	and
+	StatusChangeDate >= DATEADD(day, -366, GETDATE())
+order by
+	p.PersonId, StatusChangeDate
+
+
+
+			END
+
+
+
 
 			FETCH NEXT FROM PersonId_cursor INTO @PERSONID;
 		END
