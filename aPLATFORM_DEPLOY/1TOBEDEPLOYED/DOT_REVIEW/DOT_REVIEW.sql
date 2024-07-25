608430f34742b2048e407acaf11bc5ec8dd83cb5
Daniel Call <danny@redbonetrucking.com>
2024-07-25 08:14:51 -0600

    Update DOT_REVIEW.sql

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/DOT_REVIEW/DOT_REVIEW.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/DOT_REVIEW/DOT_REVIEW.sql
@@ -1,6 +1,6 @@
 
 select
-	p.FirstName as 'Driver First Name', p.LastName as 'Driver Last Name', 'Date of Birth (MM/DD/YYY)' as 'Date of Birth (MM/DD/YYY)', 'License State' as 'License State', 'CDL (Y/N)' as 'CDL (Y/N)',   '----------------------------------------', *,  Convert(DATE, sh.StatusChangeDate, 108) as Status_Change_Date
+	p.FirstName as 'Driver First Name', p.LastName as 'Driver Last Name', 'Date of Birth (MM/DD/YYY)' as 'Date of Birth (MM/DD/YYY)', 'License State' as 'License State', 'CDL (Y/N)' as 'CDL (Y/N)', 'asdf' as 'asdf', 'Phone Number' as 'Phone Number', 'Date of Hire (MM/DD/YYY)' as 'Date of Hire (MM/DD/YYY)',    '----------------------------------------', *,  Convert(DATE, sh.StatusChangeDate, 108) as Status_Change_Date
 from
 	main.PersonHiringStatusHistory sh
 	join main.Person p on p.PersonId = sh.PersonId
