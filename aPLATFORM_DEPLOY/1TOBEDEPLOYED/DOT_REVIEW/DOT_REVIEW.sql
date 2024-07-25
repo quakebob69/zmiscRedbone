779862530362aadd9c81c3a1fdbda20a5b222d5e
Daniel Call <danny@redbonetrucking.com>
2024-07-25 08:13:44 -0600

    d

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/DOT_REVIEW/DOT_REVIEW.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/DOT_REVIEW/DOT_REVIEW.sql
@@ -1,13 +1,10 @@
 
-select	*
-
-
-
--- Convert(DATE, sh.StatusChangeDate, 108) as HireDate  
-
-from	 main.PersonHiringStatusHistory SH
-
-where StatusChangeDate
-
+select
+	p.FirstName as 'Driver First Name', p.LastName as 'Driver Last Name', 'Date of Birth (MM/DD/YYY)' as 'Date of Birth (MM/DD/YYY)', 'License State' as 'License State', 'CDL (Y/N)' as 'CDL (Y/N)',   '----------------------------------------', *,  Convert(DATE, sh.StatusChangeDate, 108) as Status_Change_Date
+from
+	main.PersonHiringStatusHistory sh
+	join main.Person p on p.PersonId = sh.PersonId
+where
+	StatusChangeDate >= DATEADD(day, -366, GETDATE())
 order by
-PersonId, StatusChangeDate
+	p.PersonId, StatusChangeDate
