46c20ff65aab1fe01ef6e01e89d25c3655017758
Daniel Call <danny@redbonetrucking.com>
2024-07-25 12:18:30 -0600

    D

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/DOT_REVIEW/DOT_REVIEW.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/DOT_REVIEW/DOT_REVIEW.sql
@@ -43,8 +43,8 @@ DECLARE @IDList TABLE (PersonId INT);
 
 INSERT INTO @IDList (PersonId)
 SELECT
-	--DISTINCT TOP 30 p.personid
-	p.personid
+	--TOP 30 p.personid
+	DISTINCT p.personid
 from
 	main.PersonHiringStatusHistory sh
 	join main.PersonHiringStatusType phst on phst.PersonHiringStatusTypeId = sh.PersonHiringStatusTypeId
