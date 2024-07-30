58757ba438f647424f1d71d3d1c4be7bea4cd265
Daniel Call <danny@redbonetrucking.com>
2024-07-30 13:39:21 -0600

    Update DriverManager__INOUT.sql

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/DriverManager__INOUT/DriverManager__INOUT.sql
@@ -741,8 +741,8 @@
 	INSERT INTO [dispatch].[DispatchFleetManager] ([DispatchFleetManagerId], [Name], [PersonId], [PhoneNumber]) VALUES (1, 'Driver Manager 1' , 45, '(801) 111-1111')
 	INSERT INTO [dispatch].[DispatchFleetManager] ([DispatchFleetManagerId], [Name], [PersonId], [PhoneNumber]) VALUES (2, 'Driver Manager 2' , 2467, '(801) 222-2222')
 	INSERT INTO [dispatch].[DispatchFleetManager] ([DispatchFleetManagerId], [Name], [PersonId], [PhoneNumber]) VALUES (3, 'Driver Manager 3' , 2484, '(801) 333-3333')
-	INSERT INTO [dispatch].[DispatchFleetManager] ([DispatchFleetManagerId], [Name], [PersonId], [PhoneNumber]) VALUES (4, 'Driver Manager 4' , 2775, '(801) 444-4444')
-	INSERT INTO [dispatch].[DispatchFleetManager] ([DispatchFleetManagerId], [Name], [PersonId], [PhoneNumber]) VALUES (5, 'Driver Manager 5' , 6, '(801) 555-5555')
+	INSERT INTO [dispatch].[DispatchFleetManager] ([DispatchFleetManagerId], [Name], [PersonId], [PhoneNumber]) VALUES (4, 'Driver Manager 4' , 6, '(801) 444-4444')
+	--INSERT INTO [dispatch].[DispatchFleetManager] ([DispatchFleetManagerId], [Name], [PersonId], [PhoneNumber]) VALUES (5, 'Driver Manager 5' , 2775, '(801) 555-5555')
 	GO
 
 
