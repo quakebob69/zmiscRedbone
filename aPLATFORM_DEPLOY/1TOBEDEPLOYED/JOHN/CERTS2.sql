76f7c66a9977735dc7968727a5c4becbbdf13e64
Daniel Call <danny@redbonetrucking.com>
2024-08-07 12:48:46 -0600

    d

--- aPLATFORM_DEPLOY/1TOBEDEPLOYED/JOHN/CERTS2.sql
+++ aPLATFORM_DEPLOY/1TOBEDEPLOYED/JOHN/CERTS2.sql
@@ -2,7 +2,7 @@ SELECT
 main.CertificationPersonMapping.CertificationPersonMappingId, main.CertificationPersonMapping.PersonId, main.CertificationPersonMapping.CertificationTypeId, main.CertificationPersonMapping.ReceivedDate,                           main.CertificationPersonMapping.ExpireDate, main.CertificationPersonMapping.Notes, main.CertificationPersonMapping.CertificationCode, main.CertificationType.CertificationDescription, main.CertificationPersonMapping.fileName
 FROM            main.CertificationPersonMapping
 INNER JOIN                          main.CertificationType ON main.CertificationPersonMapping.CertificationTypeId = main.CertificationType.CertificationTypeId
-WHERE        (main.CertificationPersonMapping.PersonId = 1661)
+WHERE        (main.CertificationPersonMapping.PersonId = 2755)
 
 
 
