fee8aa5823adb2a8de06c550510d11186b2d5b91
Daniel Call <danny@redbonetrucking.com>
2024-09-30 14:36:16 -0600

    D

--- SQL/db_size_MBs.sql
+++ SQL/db_size_MBs.sql
@@ -4,3 +4,15 @@ SELECT
 FROM sys.master_files
 WHERE DB_NAME(database_id) = 'RedBone'
 GROUP BY database_id
+
+
+
+
+
+
+SELECT 
+    DB_NAME(database_id) AS DatabaseName,
+    CAST(SUM(CASE WHEN type_desc = 'ROWS' THEN size END) * 8. / 1024 AS DECIMAL(10,2)) AS DataSizeMB
+FROM sys.master_files
+WHERE DB_NAME(database_id) = 'ABL2.0'
+GROUP BY database_id
