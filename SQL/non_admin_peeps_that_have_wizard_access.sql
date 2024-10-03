eacb2156136b9ffa12a655c89374625df77291ca
Daniel Call <danny@redbonetrucking.com>
2024-10-03 07:48:43 -0600

    g

--- SQL/non_admin_peeps_that_have_wizard_access.sql
+++ SQL/non_admin_peeps_that_have_wizard_access.sql
@@ -1,25 +1,29 @@
-select
---pt.*, p.*, '--------', pem.*
-from main.PersonEntitlementMapping pem
-join main.Person p on p.PersonId = pem.PersonId
-join main.PersonTypeMapping ptm on ptm.PersonId = p.PersonId
-join main.PersonTypes pt on pt.PersonTypeId = ptm.PersonTypeId
-where
-pt.PersonTypeId
-in 
-(select PersonTypeId from main.PersonTypes where PersonType like '%dmin')
-
-
-
-and 
-
-PersonTypeId
-
-
-
---where EntitlementId = (select EntitlementId from main.Entitlement where EntitlementName = 'Platform Access')
-
-
-select count(*) from main.person;
-
-
+select * from main.PersonEntitlementMapping
+	where personid in
+		(
+			select
+				distinct p.personid
+				--*
+			from
+				main.person p
+			where
+				p.personid not in 
+					(
+						--admin peeps
+						select
+						distinct(p.PersonId)
+						from main.PersonEntitlementMapping pem
+						join main.Person p on p.PersonId = pem.PersonId
+						join main.PersonTypeMapping ptm on ptm.PersonId = p.PersonId
+						join main.PersonTypes pt on pt.PersonTypeId = ptm.PersonTypeId
+						where
+						pt.PersonTypeId
+						in 
+						(select PersonTypeId from main.PersonTypes where PersonType like '%dmin')
+					)
+				and
+					p.IsActive = 1
+		)
+	and
+		EntitlementId
+			= (select EntitlementId from main.Entitlement where EntitlementName = 'Platform Access')
