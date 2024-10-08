608b5025f74813d7663037d2f3194576702370f0
Daniel Call <danny@redbonetrucking.com>
2024-10-08 10:36:01 -0600

    d

--- SQL/aaaTimeClockEntry_late_repot.sql
+++ SQL/aaaTimeClockEntry_late_repot.sql
@@ -1,25 +1,45 @@
 DECLARE @emailName NVARCHAR(25) = 'danny';
 
-WITH DailyRecords AS (
-  SELECT ClockIn,
+
+--Avg Arrive
+	WITH DailyArriveRecords AS (
+	  SELECT ClockIn,
+			 ROW_NUMBER() OVER (
+			   PARTITION BY CAST(ClockIn AS DATE) 
+			   ORDER BY ClockIn
+			 ) AS RowNum
+	  FROM [RedBone].[main].[TimeClockEntry]
+	  WHERE personid IN (SELECT PersonId 
+						 FROM main.person 
+						 WHERE LoginId = @emailName + '@redbonetrucking.com')
+		AND DATEPART(HOUR, ClockIn) <= 8
+	)
+	SELECT CAST(
+	  DATEADD(
+		MILLISECOND,
+		AVG(CAST(DATEDIFF(MILLISECOND, '00:00:00', CAST(ClockIn AS TIME)) AS BIGINT)),
+		'00:00:00'
+	  ) AS TIME
+	) AS AverageArriveTime
+	FROM DailyArriveRecords
+	WHERE RowNum = 1;
+
+
+
+--Avg Leave
+WITH DailyLeaveRecords AS (
+  SELECT *,
          ROW_NUMBER() OVER (
-           PARTITION BY CAST(ClockIn AS DATE) 
-           ORDER BY ClockIn
+           PARTITION BY CAST(ClockOut AS DATE) 
+           ORDER BY ClockOut DESC
          ) AS RowNum
-  FROM [RedBone].[main].[TimeClockEntry]
-  WHERE personid IN (SELECT PersonId 
-                     FROM main.person 
-                     WHERE LoginId = @emailName + '@redbonetrucking.com')
-    AND DATEPART(HOUR, ClockIn) <= 8
+	  FROM [RedBone].[main].[TimeClockEntry]
+	  WHERE personid IN (SELECT PersonId 
+						 FROM main.person 
+						 WHERE LoginId = @emailName + '@redbonetrucking.com')
+		AND DATEPART(HOUR, ClockOut) >= 13
 )
-
-
-SELECT CAST(
-  DATEADD(
-    MILLISECOND,
-    AVG(CAST(DATEDIFF(MILLISECOND, '00:00:00', CAST(ClockIn AS TIME)) AS BIGINT)),
-    '00:00:00'
-  ) AS TIME
-) AS AverageArriveTime
-FROM DailyRecords
+SELECT *
+FROM DailyLeaveRecords
 WHERE RowNum = 1
+ORDER BY ClockOut;
