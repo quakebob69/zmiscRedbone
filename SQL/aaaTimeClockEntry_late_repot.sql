f568bab5ab4136c32779c875da8175465c0fc849
Daniel Call <danny@redbonetrucking.com>
2024-10-08 10:20:30 -0600

    D

--- SQL/aaaTimeClockEntry_late_repot.sql
+++ SQL/aaaTimeClockEntry_late_repot.sql
@@ -2,16 +2,24 @@
 DECLARE @emailName NVARCHAR(25) = 'danny';
 
 WITH DailyRecords AS (
-  SELECT *,
+  SELECT ClockIn,
          ROW_NUMBER() OVER (
            PARTITION BY CAST(ClockIn AS DATE) 
            ORDER BY ClockIn
          ) AS RowNum
   FROM [RedBone].[main].[TimeClockEntry]
-)
+  WHERE
+			personid in (SELECT PersonId FROM main.person WHERE LoginId = @emailName + '@redbonetrucking.com'
+							and
+			DATEPART(HOUR, ClockIn) <= 8))
+
+
+
 
 SELECT *
 FROM DailyRecords
 WHERE RowNum = 1
 ORDER BY ClockIn;	
+
+
 
