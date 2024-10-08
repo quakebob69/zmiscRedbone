46537b074e8697ed1f9ac38daa7ac790df60b2b8
Daniel Call <danny@redbonetrucking.com>
2024-10-08 10:28:15 -0600

    d

--- SQL/aaaTimeClockEntry_late_repot.sql
+++ SQL/aaaTimeClockEntry_late_repot.sql
@@ -1,4 +1,3 @@
---218
 DECLARE @emailName NVARCHAR(25) = 'danny';
 
 WITH DailyRecords AS (
@@ -8,34 +7,19 @@ WITH DailyRecords AS (
            ORDER BY ClockIn
          ) AS RowNum
   FROM [RedBone].[main].[TimeClockEntry]
-  WHERE
-			personid in (SELECT PersonId FROM main.person WHERE LoginId = @emailName + '@redbonetrucking.com'
-							and
-			DATEPART(HOUR, ClockIn) <= 8))
-
-
-
-/*
-SELECT *
-FROM DailyRecords
-WHERE RowNum = 1
-ORDER BY ClockIn;	
-*/
+  WHERE personid IN (SELECT PersonId 
+                     FROM main.person 
+                     WHERE LoginId = @emailName + '@redbonetrucking.com')
+    AND DATEPART(HOUR, ClockIn) <= 8
+)
 
 
 SELECT CAST(
   DATEADD(
     MILLISECOND,
-    AVG(DATEDIFF(MILLISECOND, '00:00:00', CAST(ClockIn AS TIME))),
+    AVG(CAST(DATEDIFF(MILLISECOND, '00:00:00', CAST(ClockIn AS TIME)) AS BIGINT)),
     '00:00:00'
   ) AS TIME
-) AS AverageTime
+) AS AverageArriveTime
 FROM DailyRecords
-
-
---SELECT CAST(AVG(CAST(ClockIn AS FLOAT)) AS DATETIME) AS AverageDateTime FROM DailyRecords
-
-
-
-
-
+WHERE RowNum = 1
