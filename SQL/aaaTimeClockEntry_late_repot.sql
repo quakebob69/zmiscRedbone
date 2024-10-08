eb8bc93e755f99aa41ae8195e811c40e7e5524a7
Daniel Call <danny@redbonetrucking.com>
2024-10-08 13:07:33 -0600

    D

--- SQL/aaaTimeClockEntry_late_repot.sql
+++ SQL/aaaTimeClockEntry_late_repot.sql
@@ -59,10 +59,6 @@ DECLARE @AverageLeaveTime TIME;
 
 
 
-
-
-
-
 --**************************************************************************
 	SELECT @AverageArriveTime as 'Average Arrive Time';
 	SELECT @AverageLeaveTime as 'Average Leave Time';
@@ -73,48 +69,55 @@ DECLARE @AverageLeaveTime TIME;
 
 
 
---++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
---Avg Arrive details
-	WITH DailyArriveRecords AS (
-	  SELECT ClockIn,
-			 ROW_NUMBER() OVER (
-			   PARTITION BY CAST(ClockIn AS DATE) 
-			   ORDER BY ClockIn
-			 ) AS RowNum
-	  FROM [RedBone].[main].[TimeClockEntry]
-	  WHERE personid IN (SELECT PersonId 
-						 FROM main.person 
-						 WHERE LoginId = @emailName + '@redbonetrucking.com')
-		AND DATEPART(HOUR, ClockIn) <= 9
-		AND ClockIn >= '2023-11-30 07:28:30.210'
-		AND ClockIn <= '2024-10-09 00:28:30.210'
-	)
-	SELECT *
-	FROM DailyArriveRecords
-	WHERE RowNum = 1
-	ORDER BY ClockIn;	
 
 
 
 
---Avg Leave details
-	WITH DailyLeaveRecords AS (
-	  SELECT *,
-			 ROW_NUMBER() OVER (
-			   PARTITION BY CAST(ClockOut AS DATE) 
-			   ORDER BY ClockOut DESC
-			 ) AS RowNum
+
+
+--DETAILS
+	--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+	--Avg Arrive details
+		WITH DailyArriveRecords AS (
+		  SELECT ClockIn,
+				 ROW_NUMBER() OVER (
+				   PARTITION BY CAST(ClockIn AS DATE) 
+				   ORDER BY ClockIn
+				 ) AS RowNum
 		  FROM [RedBone].[main].[TimeClockEntry]
 		  WHERE personid IN (SELECT PersonId 
 							 FROM main.person 
 							 WHERE LoginId = @emailName + '@redbonetrucking.com')
-			AND DATEPART(HOUR, ClockOut) >= 14
+			AND DATEPART(HOUR, ClockIn) <= 9
 			AND ClockIn >= '2023-11-30 07:28:30.210'
 			AND ClockIn <= '2024-10-09 00:28:30.210'
-	)
-	SELECT *
-	FROM DailyLeaveRecords
-	WHERE RowNum = 1
-	ORDER BY ClockOut;	
+		)
+		SELECT *
+		FROM DailyArriveRecords
+		WHERE RowNum = 1
+		ORDER BY ClockIn;	
+
+
+
+
+	--Avg Leave details
+		WITH DailyLeaveRecords AS (
+		  SELECT *,
+				 ROW_NUMBER() OVER (
+				   PARTITION BY CAST(ClockOut AS DATE) 
+				   ORDER BY ClockOut DESC
+				 ) AS RowNum
+			  FROM [RedBone].[main].[TimeClockEntry]
+			  WHERE personid IN (SELECT PersonId 
+								 FROM main.person 
+								 WHERE LoginId = @emailName + '@redbonetrucking.com')
+				AND DATEPART(HOUR, ClockOut) >= 14
+				AND ClockIn >= '2023-11-30 07:28:30.210'
+				AND ClockIn <= '2024-10-09 00:28:30.210'
+		)
+		SELECT *
+		FROM DailyLeaveRecords
+		WHERE RowNum = 1
+		ORDER BY ClockOut;	
 
