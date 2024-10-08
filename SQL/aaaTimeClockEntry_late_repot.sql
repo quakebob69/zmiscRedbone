e38af21b8941caa30dc5006aeb7211cf06295751
Daniel Call <danny@redbonetrucking.com>
2024-10-08 13:09:04 -0600

    D

--- SQL/aaaTimeClockEntry_late_repot.sql
+++ SQL/aaaTimeClockEntry_late_repot.sql
@@ -3,72 +3,67 @@ DECLARE @AverageArriveTime TIME;
 DECLARE @AverageLeaveTime TIME;
 
 
-
---Avg Arrive
-	WITH DailyArriveRecords AS (
-	  SELECT ClockIn,
-			 ROW_NUMBER() OVER (
-			   PARTITION BY CAST(ClockIn AS DATE) 
-			   ORDER BY ClockIn
-			 ) AS RowNum
-	  FROM [RedBone].[main].[TimeClockEntry]
-	  WHERE personid IN (SELECT PersonId 
-						 FROM main.person 
-						 WHERE LoginId = @emailName + '@redbonetrucking.com')
-		AND DATEPART(HOUR, ClockIn) <= 9
-		AND ClockIn >= '2023-11-30 07:28:30.210'
-		AND ClockIn <= '2024-10-09 00:28:30.210'
-
-	)
-	SELECT @AverageArriveTime = CAST(
-	  DATEADD(
-		MILLISECOND,
-		AVG(CAST(DATEDIFF(MILLISECOND, '00:00:00', CAST(ClockIn AS TIME)) AS BIGINT)),
-		'00:00:00'
-	  ) AS TIME
-	) 
-	FROM DailyArriveRecords
-	WHERE RowNum = 1;
-
-
-
---Avg Leave
-	WITH DailyLeaveRecords AS (
-	  SELECT *,
-			 ROW_NUMBER() OVER (
-			   PARTITION BY CAST(ClockOut AS DATE) 
-			   ORDER BY ClockOut DESC
-			 ) AS RowNum
+--SUMMARY
+	--Avg Arrive
+		WITH DailyArriveRecords AS (
+		  SELECT ClockIn,
+				 ROW_NUMBER() OVER (
+				   PARTITION BY CAST(ClockIn AS DATE) 
+				   ORDER BY ClockIn
+				 ) AS RowNum
 		  FROM [RedBone].[main].[TimeClockEntry]
 		  WHERE personid IN (SELECT PersonId 
 							 FROM main.person 
 							 WHERE LoginId = @emailName + '@redbonetrucking.com')
-			AND DATEPART(HOUR, ClockOut) >= 14
+			AND DATEPART(HOUR, ClockIn) <= 9
 			AND ClockIn >= '2023-11-30 07:28:30.210'
 			AND ClockIn <= '2024-10-09 00:28:30.210'
-	)
-	SELECT @AverageLeaveTime = CAST(
-		DATEADD(
-		MILLISECOND,
-		AVG(CAST(DATEDIFF(MILLISECOND, '00:00:00', CAST(ClockOut AS TIME)) AS BIGINT)),
-		'00:00:00'
-		) AS TIME
-	)
-	FROM DailyLeaveRecords
-	WHERE RowNum = 1;
+
+		)
+		SELECT @AverageArriveTime = CAST(
+		  DATEADD(
+			MILLISECOND,
+			AVG(CAST(DATEDIFF(MILLISECOND, '00:00:00', CAST(ClockIn AS TIME)) AS BIGINT)),
+			'00:00:00'
+		  ) AS TIME
+		) 
+		FROM DailyArriveRecords
+		WHERE RowNum = 1;
 
 
 
---**************************************************************************
-	SELECT @AverageArriveTime as 'Average Arrive Time';
-	SELECT @AverageLeaveTime as 'Average Leave Time';
-	SELECT ROUND(CAST(DATEDIFF(SECOND, @AverageArriveTime, @AverageLeaveTime) AS FLOAT) / 3600.0, 2) AS 'hours worked per day';
---**************************************************************************
+	--Avg Leave
+		WITH DailyLeaveRecords AS (
+		  SELECT *,
+				 ROW_NUMBER() OVER (
+				   PARTITION BY CAST(ClockOut AS DATE) 
+				   ORDER BY ClockOut DESC
+				 ) AS RowNum
+			  FROM [RedBone].[main].[TimeClockEntry]
+			  WHERE personid IN (SELECT PersonId 
+								 FROM main.person 
+								 WHERE LoginId = @emailName + '@redbonetrucking.com')
+				AND DATEPART(HOUR, ClockOut) >= 14
+				AND ClockIn >= '2023-11-30 07:28:30.210'
+				AND ClockIn <= '2024-10-09 00:28:30.210'
+		)
+		SELECT @AverageLeaveTime = CAST(
+			DATEADD(
+			MILLISECOND,
+			AVG(CAST(DATEDIFF(MILLISECOND, '00:00:00', CAST(ClockOut AS TIME)) AS BIGINT)),
+			'00:00:00'
+			) AS TIME
+		)
+		FROM DailyLeaveRecords
+		WHERE RowNum = 1;
 
 
 
-
-
+	--**************************************************************************
+		SELECT @AverageArriveTime as 'Average Arrive Time';
+		SELECT @AverageLeaveTime as 'Average Leave Time';
+		SELECT ROUND(CAST(DATEDIFF(SECOND, @AverageArriveTime, @AverageLeaveTime) AS FLOAT) / 3600.0, 2) AS 'hours worked per day';
+	--**************************************************************************
 
 
 
